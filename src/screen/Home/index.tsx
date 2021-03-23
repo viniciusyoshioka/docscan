e03cf2dd62f5fc49cb31188bc863077e7d9ab99a
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-03-23 12:59:28 -0300

    Added support in Home screen to read and show document

--- src/screen/Home/index.tsx
+++ src/screen/Home/index.tsx
@@ -1,14 +1,42 @@
-import React from "react"
-import { View } from "react-native"
+import React, { useCallback, useEffect, useState } from "react"
+import { FlatList, View } from "react-native"
 
 import { SafeScreen } from "../../component/Screen"
+import { Document } from "../../service/object-types"
+import { readDocument } from "../../service/storage"
 import HomeHeader from "./Header"
 
 
 export default function Home() {
+
+
+    const [document, setDocument] = useState<Array<Document>>([])
+
+
+    const getDocument = useCallback(async () => {
+        const readDocumentList = await readDocument()
+        setDocument(readDocumentList)
+    }, [])
+
+
+    useEffect(() => {
+        getDocument()
+    }, [])
+
+
     return (
         <SafeScreen>
             <HomeHeader />
+
+            <FlatList
+                data={document}
+                renderItem={({ item }) => {
+                    return (
+                        <View />
+                    )
+                }}
+                keyExtractor={(item, index) => index.toString()}
+            />
         </SafeScreen>
     )
 }
