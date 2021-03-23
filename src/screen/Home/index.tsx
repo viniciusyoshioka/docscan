a02357d10cf4979a8f24653236b86731a827272b
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-03-23 14:06:51 -0300

    Home screen uses DocumentItem to show document

--- src/screen/Home/index.tsx
+++ src/screen/Home/index.tsx
@@ -1,6 +1,8 @@
 import React, { useCallback, useEffect, useState } from "react"
-import { FlatList, View } from "react-native"
+import { BackHandler, FlatList, ToastAndroid } from "react-native"
+import { useNavigation } from "@react-navigation/core"
 
+import { DocumentItem } from "../../component/DocumentItem"
 import { SafeScreen } from "../../component/Screen"
 import { Document } from "../../service/object-types"
 import { readDocument } from "../../service/storage"
@@ -10,7 +12,11 @@ import HomeHeader from "./Header"
 export default function Home() {
 
 
+    const navigation = useNavigation()
+
     const [document, setDocument] = useState<Array<Document>>([])
+    const [selectionMode, setSelectionMode] = useState(false)
+    const [selectedDocument, setSelectedDocument] = useState<Array<number>>([])
 
 
     const getDocument = useCallback(async () => {
@@ -18,10 +24,78 @@ export default function Home() {
         setDocument(readDocumentList)
     }, [])
 
+    const openDocument = useCallback((documentObject: Document) => {
+        ToastAndroid.show("Open document is not available yet", 10)
+    }, [])
+
+    const selectDocument = useCallback((documentId: number) => {
+        if (!selectionMode) {
+            setSelectionMode(true)
+        }
+        if (selectedDocument.indexOf(documentId) === -1) {
+            selectedDocument.push(documentId)
+        }
+    }, [selectionMode, selectedDocument])
+
+    const deselectDocument = useCallback((documentId: number) => {
+        const index = selectedDocument.indexOf(documentId)
+        if (index !== -1) {
+            selectedDocument.splice(index, 1)
+        }
+        if (selectionMode && selectedDocument.length === 0) {
+            setSelectionMode(false)
+        }
+    }, [selectedDocument, selectionMode])
+
+    const setBackhandler = useCallback(() => {
+        if (selectionMode) {
+            setSelectedDocument([])
+            setSelectionMode(false)
+        } else {
+            BackHandler.exitApp()
+        }
+        return true
+    }, [selectionMode])
+
 
     useEffect(() => {
         getDocument()
     }, [])
+
+    useEffect(() => {
+        BackHandler.addEventListener("hardwareBackPress", setBackhandler)
+
+        return () => {
+            BackHandler.removeEventListener("hardwareBackPress", setBackhandler)
+        }
+    }, [selectionMode])
+
+    useEffect(() => {
+        navigation.addListener("focus", setBackhandler)
+
+        return () => {
+            navigation.removeListener("focus", setBackhandler)
+        }
+    }, [])
+
+    useEffect(() => {
+        navigation.addListener("blur", () => {
+            BackHandler.removeEventListener("hardwareBackPress", setBackhandler)
+        })
+
+        return () => {
+            navigation.removeListener("blur", () => {
+                BackHandler.removeEventListener("hardwareBackPress", setBackhandler)
+            })
+        }
+    }, [])
+
+    useEffect(() => {
+        if (selectionMode) {
+            setSelectedDocument([])
+            setSelectionMode(false)
+        }
+    }, [document])
 
 
     return (
@@ -30,12 +104,16 @@ export default function Home() {
 
             <FlatList
                 data={document}
-                renderItem={({ item }) => {
-                    return (
-                        <View />
-                    )
-                }}
-                keyExtractor={(item, index) => index.toString()}
+                renderItem={({ item }) => (
+                    <DocumentItem
+                        click={() => openDocument(item)}
+                        select={() => selectDocument(item.id)}
+                        deselect={() => deselectDocument(item.id)}
+                        selectionMode={selectionMode}
+                        document={item}
+                    />
+                )}
+                keyExtractor={(item) => item.id.toString()}
             />
         </SafeScreen>
     )
