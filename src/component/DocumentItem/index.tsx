c0e3d4dfff3657644c13dd7a9dac619deb7d1e71
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-03-23 14:05:05 -0300

    Added component DocumentItem

---
+++ src/component/DocumentItem/index.tsx
@@ -0,0 +1,150 @@
+import React, { useCallback, useContext, useEffect, useState } from "react"
+import { Animated, View } from "react-native"
+import { LongPressGestureHandler, State } from "react-native-gesture-handler"
+import CheckBox from "@react-native-community/checkbox"
+
+import { Document } from "../../service/object-types"
+import { ThemeContext } from "../../service/theme"
+import { Block, Button, Date, Line, Title } from "./style"
+
+
+export interface DocumentItemProps {
+    click: () => void,
+    select: () => void,
+    deselect: () => void,
+    selectionMode: boolean,
+    document: Document,
+}
+
+
+export function DocumentItem(props: DocumentItemProps) {
+
+
+    const { color } = useContext(ThemeContext)
+
+    const animatedWidth = new Animated.Value(0)
+    const animatedOpacity = new Animated.Value(0)
+
+    const [selected, setSelected] = useState(false)
+
+
+    const normalPress = useCallback(() => {
+        if (props.selectionMode === false) {
+            props.click()
+        } else if (selected === false) {
+            props.select()
+            setSelected(true)
+        } else if (selected === true) {
+            props.deselect()
+            setSelected(false)
+        }
+    }, [props.selectionMode, selected, props.click])
+
+    const longPress = useCallback((nativeEvent) => {
+        if (nativeEvent.state === State.ACTIVE) {
+            if (props.selectionMode === false) {
+                props.select()
+                setSelected(true)
+            }
+        }
+    }, [props.selectionMode])
+
+
+    useEffect(() => {
+        if (props.selectionMode === true) {
+            Animated.sequence([
+                Animated.parallel([
+                    Animated.timing(animatedWidth, {
+                        toValue: 35,
+                        duration: 50,
+                        useNativeDriver: false
+                    }),
+                    Animated.timing(animatedOpacity, {
+                        toValue: 1,
+                        duration: 50,
+                        useNativeDriver: false
+                    })
+                ])
+            ]).start()
+        } else if (props.selectionMode === false) {
+            if (selected === false) {
+                Animated.sequence([
+                    Animated.parallel([
+                        Animated.timing(animatedWidth, {
+                            toValue: 0,
+                            duration: 50,
+                            useNativeDriver: false
+                        }),
+                        Animated.timing(animatedOpacity, {
+                            toValue: 0,
+                            duration: 50,
+                            useNativeDriver: false
+                        })
+                    ])
+                ]).start()
+            } else {
+                setSelected(false)
+            }
+        }
+    }, [props.selectionMode])
+
+    useEffect(() => {
+        if (props.selectionMode === false && selected === false) {
+            Animated.sequence([
+                Animated.parallel([
+                    Animated.timing(animatedWidth, {
+                        toValue: 0,
+                        duration: 50,
+                        useNativeDriver: false
+                    }),
+                    Animated.timing(animatedOpacity, {
+                        toValue: 0,
+                        duration: 50,
+                        useNativeDriver: false
+                    })
+                ])
+            ]).start()
+        }
+    }, [selected])
+
+
+    return (
+        <LongPressGestureHandler 
+            maxDist={30} 
+            minDurationMs={800} 
+            onHandlerStateChange={({ nativeEvent }) => longPress(nativeEvent)}
+        >
+            <Button onPress={() => normalPress()}>
+                <Animated.View 
+                    style={[Block, {
+                        width: animatedWidth, opacity: animatedOpacity, 
+                        marginRight: 5
+                    }]}
+                >
+                    <CheckBox 
+                        value={selected}
+                        onChange={() => normalPress()}
+                        tintColors={{
+                            true: color.icon,
+                            false: color.icon
+                        }}
+                    />
+                </Animated.View>
+
+                <View style={[Block, {alignItems: "flex-start"}]}>
+                    <Line>
+                        <Title numberOfLines={1}>
+                            {props.document.name}
+                        </Title>
+                    </Line>
+
+                    <Line>
+                        <Date numberOfLines={1}>
+                            {props.document.lastModificationDate}
+                        </Date>
+                    </Line>
+                </View>
+            </Button>
+        </LongPressGestureHandler>
+    )
+}
---
+++ src/component/DocumentItem/style.tsx
@@ -0,0 +1,46 @@
+import { StyleSheet } from "react-native"
+import { RectButton } from "react-native-gesture-handler"
+import styled from "styled-components/native"
+
+import { styledProps } from "../../service/theme"
+
+
+export const Button = styled(RectButton)`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+    margin: 3px;
+    padding: 3px 5px;
+    height: 55px;
+    border-radius: 1px;
+    background-color: ${(props: styledProps) =>  props.theme.color.backgroundDark};
+`
+
+
+export const Block = StyleSheet.create({
+    block: {
+        alignItems: "center",
+        justifyContent: "center"
+    }
+}).block
+
+
+export const Line = styled.View`
+    display: flex;
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+`
+
+
+export const Title = styled.Text`
+    font-size: 16px;
+    color: ${(props: styledProps) => props.theme.color.color};
+`
+
+
+export const Date = styled.Text`
+    font-size: 12px;
+    color: ${(props: styledProps) => props.theme.color.colorDark};
+`
