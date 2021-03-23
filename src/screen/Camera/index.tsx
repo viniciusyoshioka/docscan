f48ef3fa1321cd98a0c716ede42d505f0684509f
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-03-23 20:52:58 -0300

    Added CameraControl

--- src/screen/Camera/index.tsx
+++ src/screen/Camera/index.tsx
@@ -1,20 +1,36 @@
 import React, { useCallback, useEffect, useRef, useState } from "react"
-import { BackHandler } from "react-native"
+import { Alert, BackHandler, ToastAndroid } from "react-native"
 import { RNCamera } from "react-native-camera"
-import { useNavigation } from "@react-navigation/native"
+import { RouteProp, useNavigation, useRoute } from "@react-navigation/native"
 import Orientation from "react-native-orientation-locker"
+import Icon from "react-native-vector-icons/Ionicons"
 
 import { SafeScreen } from "../../component/Screen"
 import CameraHeader from "./Header"
 import CameraSettings from "./CameraSetings"
-import { settingsDefaultCamera } from "../../service/constant"
+import { fullPathPictureOriginal, settingsDefaultCamera } from "../../service/constant"
 import { readSettings } from "../../service/storage"
+import { CameraControlButtonBase, cameraControlIconSize, CameraControlView } from "../../component/CameraControl"
+import { createAllFolder } from "../../service/folder-handler"
+import { getDateTime } from "../../service/date"
+import { getDocumentName } from "../../service/document-handler"
+import { Document } from "../../service/object-types"
+
+
+type CameraParams = {
+    Camera: {
+        newPictureList: Array<string>,
+        newDocumentName: string,
+        documentObject: Document,
+    }
+}
 
 
 export default function Camera() {
 
 
     const navigation = useNavigation()
+    const { params } = useRoute<RouteProp<CameraParams, "Camera">>()
     const cameraRef = useRef<RNCamera>(null)
 
     const [cameraSettingsVisible, setCameraSettingsVisible] = useState(false)
@@ -22,6 +38,10 @@ export default function Camera() {
     const [flash, setFlash] = useState(cameraSettings.flash)
     const [focus, setFocus] = useState(cameraSettings.focus)
     const [whiteBalance, setWhiteBalance] = useState(cameraSettings.whiteBalance)
+
+    const [documentName, setDocumentName] = useState("Documento vazio")
+    const [pictureList, setPictureList] = useState<Array<string>>([])
+    const [documentObject, setDocumentObject] = useState<Document | undefined>(undefined)
 
 
     const readCameraSettings = useCallback(async () => {
@@ -35,27 +55,83 @@ export default function Camera() {
     }, [])
 
     const goBack = useCallback(() => {
-        navigation.navigate("Home")
-    }, [])
+        if (pictureList.length === 0) {
+            navigation.navigate("Home")
+            return
+        }
+
+        Alert.alert(
+            "Este documento não foi salvo", 
+            "Sair agora descartará este documento, e esta ação não poderá ser desfeita",
+            [
+                {
+                    text: "Voltar", 
+                    onPress: () => {
+                        navigation.navigate("Home")
+                    }
+                }, 
+                {
+                    text: "Cancelar", 
+                    onPress: () => {}
+                }
+            ]
+        )
+    }, [pictureList])
 
     const backhandlerFunction = useCallback(() => {
         goBack()
         return true
-    }, [])
+    }, [goBack])
 
     const setBackhandler = useCallback(() => {
         BackHandler.addEventListener(
             "hardwareBackPress",
             backhandlerFunction
         )
-    }, [])
+    }, [backhandlerFunction])
 
     const removeBackhandler = useCallback(() => {
         BackHandler.removeEventListener(
             "hardwareBackPress",
             backhandlerFunction
         )
+    }, [backhandlerFunction])
+
+    const addPictureFromGalery = useCallback(() => {
+        ToastAndroid.show("Add picture from galery is not available yet", 10)
     }, [])
+
+    const takePicture = useCallback(async () => {
+        await createAllFolder()
+
+        const date = getDateTime("-", "-", true)
+        const picturePath = `${fullPathPictureOriginal}/${date}.jpg`
+        const options = {
+            quality: 0.3,
+            path: picturePath
+        }
+
+        try {
+            await cameraRef.current?.takePictureAsync(options)
+            setPictureList([...pictureList, picturePath])
+            if (documentName === "" || documentName === "Documento vazio") {
+                setDocumentName(getDocumentName())
+            }
+        } catch {
+            Alert.alert(
+                "Erro ao tirar foto", 
+                "Não foi possível tirar foto, ocorreu erro desconhecido"
+            )
+        }
+    }, [cameraRef, pictureList, documentName])
+
+    const editDocument = useCallback(() => {
+        navigation.navigate("EditDocument", {
+            pictureList: pictureList,
+            documentName: documentName,
+            documentObject: documentObject,
+        })
+    }, [pictureList, documentName, documentObject])
 
 
     useEffect(() => {
@@ -73,7 +149,7 @@ export default function Camera() {
         return () => {
             removeBackhandler()
         }
-    }, [])
+    }, [backhandlerFunction])
 
     useEffect(() => {
         navigation.addListener("focus", setBackhandler)
@@ -81,7 +157,7 @@ export default function Camera() {
         return () => {
             navigation.removeListener("focus", setBackhandler)
         }
-    }, [])
+    }, [setBackhandler])
 
     useEffect(() => {
         navigation.addListener("blur", removeBackhandler)
@@ -89,7 +165,15 @@ export default function Camera() {
         return () => {
             navigation.removeListener("blur", removeBackhandler)
         }
-    }, [])
+    }, [removeBackhandler])
+
+    useEffect(() => {
+        if (params !== undefined) {
+            setPictureList(params.newPictureList)
+            setDocumentName(params.newDocumentName)
+            setDocumentObject(params.documentObject)
+        }
+    }, [params])
 
 
     return (
@@ -119,6 +203,27 @@ export default function Camera() {
                 goBack={goBack} 
                 openSettings={() => setCameraSettingsVisible(true)}
             />
+
+            <CameraControlView style={{position: "absolute", bottom: 0, left: 0, right: 0, backgroundColor: "transparent"}}>
+                <CameraControlButtonBase onPress={() => addPictureFromGalery()}>
+                    <Icon 
+                        name={"md-image-outline"} 
+                        size={cameraControlIconSize} 
+                        color={"rgb(255, 255, 255)"} />
+                </CameraControlButtonBase>
+
+                <CameraControlButtonBase 
+                    onPress={async () => await takePicture()} 
+                    style={{backgroundColor: "rgb(255, 255, 255)"}}
+                />
+
+                <CameraControlButtonBase onPress={() => editDocument()}>
+                    <Icon 
+                        name={"md-document-outline"} 
+                        size={cameraControlIconSize} 
+                        color={"rgb(255, 255, 255)"} />
+                </CameraControlButtonBase>
+            </CameraControlView>
         </SafeScreen>
     )
 }
