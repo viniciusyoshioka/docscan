d904eb63fb41870e431790b96f288212120c33a0
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-03-23 16:23:08 -0300

    Created debug functionality to read, write, clear document and change theme

--- src/screen/Home/index.tsx
+++ src/screen/Home/index.tsx
@@ -1,22 +1,65 @@
-import React, { useCallback, useEffect, useState } from "react"
-import { BackHandler, FlatList, ToastAndroid } from "react-native"
+import React, { useCallback, useContext, useEffect, useState } from "react"
+import { BackHandler, FlatList, ToastAndroid, View } from "react-native"
 import { useNavigation } from "@react-navigation/core"
 
 import { DocumentItem } from "../../component/DocumentItem"
 import { SafeScreen } from "../../component/Screen"
 import { Document } from "../../service/object-types"
-import { readDocument } from "../../service/storage"
+import { readDocument, readDocumentId, writeDocument, writeDocumentId } from "../../service/storage"
 import HomeHeader from "./Header"
+import { DebugButton } from "../../component/DebugButton"
+import { SwitchThemeContext } from "../../service/theme"
+import { themeDark, themeLight } from "../../service/constant"
 
 
 export default function Home() {
 
+
+    const switchTheme = useContext(SwitchThemeContext)
 
     const navigation = useNavigation()
 
     const [document, setDocument] = useState<Array<Document>>([])
     const [selectionMode, setSelectionMode] = useState(false)
     const [selectedDocument, setSelectedDocument] = useState<Array<number>>([])
+
+
+    const debugReadDocument = useCallback(async () => {
+        const documentList = await readDocument()
+        const documentId = await readDocumentId()
+
+        setDocument(documentList)
+
+        console.log(`document - READ - ${JSON.stringify(documentList)}`)
+        console.log(`documentId - READ - ${JSON.stringify(documentId)}`)
+    }, [])
+
+    const debugWriteDocument = useCallback(async () => {
+        const tempDocumentList: Array<Document> = [
+            {id: 0, name: "Titulo 1", lastModificationDate: "09:19 08/03/2021", pictureList: []},
+            {id: 1, name: "Titulo 2", lastModificationDate: "09:19 08/03/2021", pictureList: []},
+            {id: 2, name: "Titulo 3", lastModificationDate: "09:19 08/03/2021", pictureList: []},
+            {id: 3, name: "Titulo 4", lastModificationDate: "09:19 08/03/2021", pictureList: []},
+            {id: 4, name: "Titulo 5", lastModificationDate: "09:19 08/03/2021", pictureList: []},
+        ]
+        const tempDocumentId: Array<number> = [0, 1, 2, 3, 4]
+
+        await writeDocument(tempDocumentList)
+        await writeDocumentId(tempDocumentId)
+
+        setDocument(tempDocumentList)
+
+        console.log("document, documentId - WRITTEN")
+    }, [])
+
+    const debugClearDocument = useCallback(async () => {
+        await writeDocument([])
+        await writeDocumentId([])
+        
+        setDocument([])
+
+        console.log("document, documentId - CLEAR")
+    }, [])
 
 
     const getDocument = useCallback(async () => {
@@ -115,6 +158,30 @@ export default function Home() {
                 )}
                 keyExtractor={(item) => item.id.toString()}
             />
+
+            <View>
+                <DebugButton 
+                    text={"Ler"} 
+                    onPress={async () => await debugReadDocument()} 
+                    style={{bottom: 105}} />
+                <DebugButton 
+                    text={"Escre"} 
+                    onPress={async () => await debugWriteDocument()} 
+                    style={{bottom: 60}} />
+                <DebugButton 
+                    text={"Limpar"} 
+                    onPress={async () => await debugClearDocument()} 
+                    style={{bottom: 5}} />
+
+                <DebugButton 
+                    text={"Claro"} 
+                    onPress={async () => await switchTheme(themeLight)} 
+                    style={{bottom: 60, left: 60}} />
+                <DebugButton 
+                    text={"Escuro"} 
+                    onPress={async () => await switchTheme(themeDark)} 
+                    style={{bottom: 5, left: 60}} />
+            </View>
         </SafeScreen>
     )
 }
--- src/service/constant.tsx
+++ src/service/constant.tsx
@@ -5,7 +5,7 @@ export const appInDevelopment = true
 // App
 export const appName = appInDevelopment ? "DocScanBeta" : "DocScan"
 export const appFName = appInDevelopment ? "DocScan Beta" : "DocScan"
-export const appVersion = "0.0.11"
+export const appVersion = "0.0.12"
 export const appType = appInDevelopment ? "beta" : "release"
 
 // // App icon
