c2ef51d20e7872f28929cd07d5d5fe745e113333
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-03-24 17:47:01 -0300

    Created and added Header in Settings screen and added version in Settings screen

--- src/screen/Settings/index.tsx
+++ src/screen/Settings/index.tsx
@@ -1,12 +1,63 @@
-import React from "react"
+import React, { useCallback, useEffect } from "react"
+import { BackHandler } from "react-native"
+import { useNavigation } from "@react-navigation/core"
 
 import { SafeScreen } from "../../component/Screen"
+import SettingsHeader from "./Header"
+import { TextVersion, ViewVersion } from "./style"
+import { appFName, appType, appVersion } from "../../service/constant"
 
 
 export default function Settings() {
+
+
+    const navigation = useNavigation()
+
+
+    const goBack = useCallback(() => {
+        navigation.navigate("Home")
+    }, [])
+
+    const backhandlerFunction = useCallback(() => {
+        goBack()
+        return true
+    }, [])
+
+    const setBackhandler = useCallback(() => {
+        BackHandler.addEventListener(
+            "hardwareBackPress", 
+            () => backhandlerFunction()
+        )
+    }, [])
+
+    const removeBackhandler = useCallback(() => {
+        BackHandler.removeEventListener(
+            "hardwareBackPress", 
+            () => backhandlerFunction()
+        )
+    }, [])
+
+
+    useEffect(() => {
+        setBackhandler()
+
+        return () => {
+            removeBackhandler()
+        }
+    }, [])
+
+
     return (
         <SafeScreen>
+            <SettingsHeader
+                goBack={goBack}
+            />
 
+            <ViewVersion>
+                <TextVersion>
+                    {appFName} - {appVersion} - {appType}
+                </TextVersion>
+            </ViewVersion>
         </SafeScreen>
     )
 }
