5d398692d85dd3a28452eec6013b04cae2354396
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-03-24 19:09:57 -0300

    Created and added screen EditDocument

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -5,6 +5,7 @@ import { createStackNavigator } from "@react-navigation/stack"
 import Home from "../screen/Home"
 import Camera from "../screen/Camera"
 import Settings from "../screen/Settings"
+import EditDocument from "../screen/EditDocument"
 
 
 const Stack = createStackNavigator()
@@ -17,6 +18,7 @@ export default function Router() {
                 <Stack.Screen name={"Home"} component={Home} />
                 <Stack.Screen name={"Camera"} component={Camera} />
                 <Stack.Screen name={"Settings"} component={Settings} />
+                <Stack.Screen name={"EditDocument"} component={EditDocument} />
             </Stack.Navigator>
         </NavigationContainer>
     )
---
+++ src/screen/EditDocument/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+import { SafeScreen } from "../../component/Screen"
+
+
+export default function EditDocument() {
+    return (
+        <SafeScreen>
+
+        </SafeScreen>
+    )
+}
