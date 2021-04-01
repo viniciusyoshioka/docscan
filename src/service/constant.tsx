b4b7bedcd5bccca1b95417ffc290e83c386276d6
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-03-31 23:48:00 -0300

    Updated app icon, and available version are docscan, docscanbeta and their outline variants

--- src/service/constant.tsx
+++ src/service/constant.tsx
@@ -9,14 +9,14 @@ export const appInDevelopment = true
 // App
 export const appName = appInDevelopment ? "DocScanBeta" : "DocScan"
 export const appFName = appInDevelopment ? "DocScan Beta" : "DocScan"
-export const appVersion = "0.0.79"
+export const appVersion = "0.0.80"
 export const appType = appInDevelopment ? "beta" : "release"
 
-// // App icon
+// App icon
 const appIconPath = "./../image/app/"
 const appIconImage = appInDevelopment ? "docscanbeta.png" : "docscan.png"
 const appIconImageOutline = appInDevelopment ? "docscanbeta_outline.png" : "docscan_outline.png"
-// export const appIcon = require(`${appIconPath}${appIconImage}`)
+export const appIcon = require(`${appIconPath}${appIconImage}`)
 export const appIconOutline = require(`${appIconPath}${appIconImageOutline}`)
 
 // External folder
