c3ff3df66f15f8a9671fc69ff14dc7fd7ea324dc
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-04-05 20:53:01 -0300

    Fixed bug that crash the app when opening Menu at Home screen

--- src/screen/Home/HomeMenu.tsx
+++ src/screen/Home/HomeMenu.tsx
@@ -49,7 +49,7 @@ export default function HomeMenu(props: HomeMenuProps) {
                 <HeaderButton 
                     iconName={"md-ellipsis-vertical"}
                     iconSize={22} 
-                    onPress={menuRef.current?.open}
+                    onPress={() => menuRef.current?.open()}
                 />
             </MenuTrigger>
 
