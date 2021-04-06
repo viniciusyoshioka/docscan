972c54bb4f44ea39b22c68277cfc84495403543d
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-04-05 21:32:00 -0300

    Added error treatment in PDF creator/handler

--- src/service/pdf-handler.tsx
+++ src/service/pdf-handler.tsx
@@ -1,17 +1,12 @@
-import { Alert } from "react-native"
-
 import { fullPathPdf } from "./constant"
 import PdfCreator from "./pdf-creator"
 
 
 export function createPdf(documentName: string, pictureList: Array<string>) {
     const documentPath = `${fullPathPdf}/${documentName}.pdf`
-    PdfCreator.exportPicturesToPdf(documentPath, pictureList)
-        .catch((error) => {
-            console.log(error)
-            Alert.alert(
-                "Erro exportando PDF",
-                `Mensagem de erro: "${error}"`
-            )
-        })
+    try {
+        PdfCreator.exportPicturesToPdf(documentPath, pictureList)
+    } catch (error) {
+        console.log(error)
+    }
 }
