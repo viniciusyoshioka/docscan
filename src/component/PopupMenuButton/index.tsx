bacb88264158f195ccc3e27ff82105f6d297530b
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-05-02 10:46:12 -0300

    Refactored PopupMenuButton

--- src/component/PopupMenuButton/index.tsx
+++ src/component/PopupMenuButton/index.tsx
@@ -1,23 +1,20 @@
 import React from "react"
+import { RectButtonProps } from "react-native-gesture-handler"
 
 import { PopupMenuButtonBase, PopupMenuButtonText } from "./style"
 
 
-export interface PopupMenuButtonProps {
+export interface PopupMenuButtonProps extends RectButtonProps {
     text: string,
-    onPress: () => void,
 }
 
 
 export function PopupMenuButton(props: PopupMenuButtonProps) {
     return (
-        <PopupMenuButtonBase onPress={props.onPress}>
+        <PopupMenuButtonBase {...props}>
             <PopupMenuButtonText numberOfLines={1}>
                 {props.text}
             </PopupMenuButtonText>
         </PopupMenuButtonBase>
     )
 }
-
-
-export { PopupMenuButtonBase, PopupMenuButtonText }
