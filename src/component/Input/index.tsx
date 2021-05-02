00b3d5d3abcfd247ef8a2ef42c2304c5ab5be72d
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-05-02 10:34:16 -0300

    Refactored Input

--- src/component/Input/index.tsx
+++ src/component/Input/index.tsx
@@ -1,8 +1,18 @@
 import React, { forwardRef } from "react"
 import { TextInput, TextInputProps } from "react-native"
+import styled from "styled-components/native"
 
-import { InputBase } from "./style"
-import { useTheme } from "../../service/theme"
+import { styledProps, useTheme } from "../../service/theme"
+
+
+export const InputBase = styled.TextInput`
+    font-size: 15px;
+    padding: 0px 5px;
+    width: 100%;
+    color: ${(props: styledProps) => props.theme.color.color};
+    border-bottom-width: 2px;
+    border-color: ${(props: styledProps) => props.theme.color.color};
+`
 
 
 export const Input = forwardRef((props: TextInputProps, ref?: React.Ref<TextInput>) => {
@@ -13,13 +23,10 @@ export const Input = forwardRef((props: TextInputProps, ref?: React.Ref<TextInpu
 
     return (
         <InputBase
-            ref={ref || null}
+            ref={ref}
             placeholderTextColor={props.placeholderTextColor || color.colorDark}
             blurOnSubmit={false}
             {...props}
         />
     )
 })
-
-
-export { InputBase }
