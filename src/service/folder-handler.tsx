9cccf6ae668f98e5e7fefa2fe6cc399c495736df
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-05-04 09:21:08 -0300

    folder-handler use async function without async/await

--- src/service/folder-handler.tsx
+++ src/service/folder-handler.tsx
@@ -4,79 +4,54 @@ import { fullPathRoot, fullPathPdf, fullPathPicture, fullPathPictureOriginal, fu
 import { log } from "./log"
 
 
-function createRootFolder() {
-    RNFS.exists(fullPathRoot)
-        .then((exists: boolean) => {
-            if (!exists) {
-                RNFS.mkdir(fullPathRoot)
-                    .catch((error) => {
-                        log("ERROR", `folder-handler createRootFolder - Erro ao criar pasta. Mensagem: "${error}"`)
-                    })
-            }
-        })
-        .catch((error) => {
-            log("ERROR", `folder-handler createRootFolder - Erro ao criar pasta. Mensagem: "${error}"`)
-        })
+async function createRootFolder() {
+    try {
+        if (!await RNFS.exists(fullPathRoot)) {
+            await RNFS.mkdir(fullPathRoot)
+        }
+    } catch (error) {
+        log("ERROR", `folder-handler createRootFolder - Erro ao criar pasta. Mensagem: "${error}"`)
+    }
 }
 
-function createPdfFolder() {
-    RNFS.exists(fullPathPdf)
-        .then((exists: boolean) => {
-            if (!exists) {
-                RNFS.mkdir(fullPathPdf)
-                    .catch((error) => {
-                        log("ERROR", `folder-handler createPdfFolder - Erro ao criar pasta. Mensagem: "${error}"`)
-                    })
-            }
-        })
-        .catch((error) => {
-            log("ERROR", `folder-handler createPdfFolder - Erro ao criar pasta. Mensagem: "${error}"`)
-        })
+async function createPdfFolder() {
+    try {
+        if (!await RNFS.exists(fullPathPdf)) {
+            await RNFS.mkdir(fullPathPdf)
+        }
+    } catch (error) {
+        log("ERROR", `folder-handler createPdfFolder - Erro ao criar pasta. Mensagem: "${error}"`)
+    }
 }
 
-function createPictureFolder() {
-    RNFS.exists(fullPathPicture)
-        .then((exists: boolean) => {
-            if (!exists) {
-                RNFS.mkdir(fullPathPicture)
-                    .catch((error) => {
-                        log("ERROR", `folder-handler createPictureFolder - Erro ao criar pasta. Mensagem: "${error}"`)
-                    })
-            }
-        })
-        .catch((error) => {
-            log("ERROR", `folder-handler createPictureFolder - Erro ao criar pasta. Mensagem: "${error}"`)
-        })
+async function createPictureFolder() {
+    try {
+        if (!await RNFS.exists(fullPathPicture)) {
+            await RNFS.mkdir(fullPathPicture)
+        }
+    } catch (error) {
+        log("ERROR", `folder-handler createPictureFolder - Erro ao criar pasta. Mensagem: "${error}"`)
+    }
 }
 
-function createPictureOriginalFolder() {
-    RNFS.exists(fullPathPictureOriginal)
-        .then((exists: boolean) => {
-            if (!exists) {
-                RNFS.mkdir(fullPathPictureOriginal)
-                    .catch((error) => {
-                        log("ERROR", `folder-handler createPictureOriginalFolder - Erro ao criar pasta. Mensagem: "${error}"`)
-                    })
-            }
-        })
-        .catch((error) => {
-            log("ERROR", `folder-handler createPictureOriginalFolder - Erro ao criar pasta. Mensagem: "${error}"`)
-        })
+async function createPictureOriginalFolder() {
+    try {
+        if (!await RNFS.exists(fullPathPictureOriginal)) {
+            await RNFS.mkdir(fullPathPictureOriginal)
+        }
+    } catch (error) {
+        log("ERROR", `folder-handler createPictureOriginalFolder - Erro ao criar pasta. Mensagem: "${error}"`)
+    }
 }
 
-function createPictureCroppedFolder() {
-    RNFS.exists(fullPathPictureCropped)
-        .then((exists: boolean) => {
-            if (!exists) {
-                RNFS.mkdir(fullPathPictureCropped)
-                    .catch((error) => {
-                        log("ERROR", `folder-handler createPictureCroppedFolder - Erro ao criar pasta. Mensagem: "${error}"`)
-                    })
-            }
-        })
-        .catch((error) => {
-            log("ERROR", `folder-handler createPictureCroppedFolder - Erro ao criar pasta. Mensagem: "${error}"`)
-        })
+async function createPictureCroppedFolder() {
+    try {
+        if (!await RNFS.exists(fullPathPictureCropped)) {
+            await RNFS.mkdir(fullPathPictureCropped)
+        }
+    } catch (error) {
+        log("ERROR", `folder-handler createPictureCroppedFolder - Erro ao criar pasta. Mensagem: "${error}"`)
+    }
 }
 
 export function createAllFolder() {
