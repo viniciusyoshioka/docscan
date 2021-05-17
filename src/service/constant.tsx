c5ddf95c73f94b1b9d6b7b98f2ceffa67531894b
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-05-17 08:46:25 -0300

    Updated version number

--- src/service/constant.tsx
+++ src/service/constant.tsx
@@ -8,7 +8,7 @@ export const appInDevelopment = true
 // App
 export const appName = appInDevelopment ? "DocScanBeta" : "DocScan"
 export const appFName = appInDevelopment ? "DocScan Beta" : "DocScan"
-export const appVersion = "1.9.5"
+export const appVersion = "1.9.6"
 export const appType = appInDevelopment ? "beta" : "release"
 
 // Icon
