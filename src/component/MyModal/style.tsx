45cb1f9f713ea706678864d6289ea0e3fd705651
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-05-31 21:15:41 -0300

    Created custom modal component MyModal

---
+++ src/component/MyModal/style.tsx
@@ -0,0 +1,31 @@
+import styled from "styled-components/native"
+
+import { styledProps } from "../../service/theme"
+
+
+export const ModalScreen = styled.TouchableOpacity`
+    position: absolute;
+    top: 0px;
+    bottom: 0px;
+    left: 0px;
+    right: 0px;
+    align-items: center;
+    justify-content: center;
+`
+
+
+export const ModalBackground = styled.View`
+    position: absolute;
+    top: 0px;
+    bottom: 0px;
+    left: 0px;
+    right: 0px;
+    background-color: rgba(0, 0, 0, 0.5);
+`
+
+
+export const ModalPopup = styled.TouchableOpacity`
+    width: 80%;
+    border-radius: 1px;
+    background-color: ${(props: styledProps) => props.theme.color.modal_background};
+`
