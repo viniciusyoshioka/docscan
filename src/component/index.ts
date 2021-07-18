1dda47db419708a18c97a32ea823630282d15182
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-07-18 10:15:19 -0300

    Update index of component

--- src/component/index.ts
+++ src/component/index.ts
@@ -4,10 +4,14 @@ import { AddText, AddTextProps } from "./AddText"
 import { Button, ButtonProps } from "./Button"
 import { ButtonOutline, ButtonOutlineProps } from "./ButtonOutline"
 import { ButtonText, ButtonTextProps } from "./ButtonText"
+import { CameraControlAction, CameraControlButton, CameraControlView, CameraControlButtonProps } from "./CameraControl"
+import { CameraSettingsButton, CameraSettingsButtonProps } from "./CameraSettingsButton"
 import { CheckButton, CheckButtonProps } from "./CheckButton"
 import { DebugButton, DebugButtonProps } from "./DebugButton"
+import { DocumentItem, DocumentItemProps } from "./DocumentItem"
 import { EmptyList, EmptyListProps } from "./EmptyList"
 import { Header, HeaderButton, HeaderButtonProps, HeaderTitle, HeaderTitleProps } from "./Header"
+import { ImageItem, ImageItemProps } from "./ImageItem"
 import { Input, InputProps } from "./Input"
 import { InputPassword } from "./InputPassword"
 import { InputText } from "./InputText"
@@ -15,9 +19,11 @@ import { InputTitle } from "./InputTitle"
 import { ListItem, ListItemProps } from "./ListItem"
 import { MenuItem, MenuItemProps } from "./MenuItem"
 import { Modal, ModalProps } from "./Modal"
+import { ModalCameraSettings, ModalCameraSettingsProps } from "./ModalCameraSettings"
 import { ModalButton, ModalButtonProps, ModalTitle, ModalViewButton, ModalViewContent } from "./ModalComponent"
 import { MyModal, MyModalProps } from "./MyModal"
 import { NoteItem, NoteItemProps } from "./NoteItem"
+import { PictureItem, PictureItemProps } from "./Pictureitem"
 import { RadioButton, RadioButtonProps } from "./RadioButton"
 import { CenterScreen, SafeScreen, SpaceScreen } from "./Screen"
 import { SubHeader, SubHeaderText, SubHeaderTextProps } from "./SubHeader"
@@ -35,14 +41,22 @@ export { ButtonOutline }
 export type { ButtonOutlineProps }
 export { ButtonText }
 export type { ButtonTextProps }
+export { CameraControlAction, CameraControlButton, CameraControlView }
+export type { CameraControlButtonProps }
+export { CameraSettingsButton }
+export type { CameraSettingsButtonProps }
 export { CheckButton }
 export type { CheckButtonProps }
 export { DebugButton }
 export type { DebugButtonProps }
+export { DocumentItem }
+export type { DocumentItemProps }
 export { EmptyList }
 export type { EmptyListProps }
 export { Header, HeaderButton, HeaderTitle }
 export type { HeaderButtonProps, HeaderTitleProps }
+export { ImageItem }
+export type { ImageItemProps }
 export { Input }
 export type { InputProps }
 export { InputPassword }
@@ -54,12 +68,16 @@ export { MenuItem }
 export type { MenuItemProps }
 export { Modal }
 export type { ModalProps }
+export { ModalCameraSettings }
+export type { ModalCameraSettingsProps }
 export { ModalButton, ModalTitle, ModalViewButton, ModalViewContent }
 export type { ModalButtonProps }
 export { MyModal }
 export type { MyModalProps }
 export { NoteItem }
 export type { NoteItemProps }
+export { PictureItem }
+export type { PictureItemProps }
 export { RadioButton }
 export type { RadioButtonProps }
 export { CenterScreen, SafeScreen, SpaceScreen }
