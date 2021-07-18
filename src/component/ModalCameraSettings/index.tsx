7a78f90c64c222f2c1e0423da200fe40d7a439ed
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-07-18 10:08:47 -0300

    Add component ModalCameraSettings

---
+++ src/component/ModalCameraSettings/index.tsx
@@ -0,0 +1,39 @@
+import React, { ReactNode, useCallback } from "react"
+import { Modal as RNModal, ModalProps as RNModalProps, TouchableOpacity } from "react-native"
+
+import { ModalBackground, ModalContent, ModalView } from "./style"
+
+
+export interface ModalCameraSettingsProps extends RNModalProps {
+    visible: boolean,
+    setVisible: (visible: boolean) => void,
+    onClose?: () => void,
+    children?: ReactNode,
+}
+
+
+export function ModalCameraSettings(props: ModalCameraSettingsProps) {
+
+
+    const closeModal = useCallback(() => {
+        if (props.onClose) {
+            props.onClose()
+        }
+        props.setVisible(false)
+    }, [])
+
+
+    return (
+        <RNModal transparent={true} visible={props.visible} onRequestClose={closeModal}>
+            <TouchableOpacity style={{ flex: 1 }} activeOpacity={1} onPress={closeModal}>
+                <ModalView>
+                    <ModalBackground activeOpacity={1}>
+                        <ModalContent>
+                            {props?.children}
+                        </ModalContent>
+                    </ModalBackground>
+                </ModalView>
+            </TouchableOpacity>
+        </RNModal>
+    )
+}
---
+++ src/component/ModalCameraSettings/style.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components/native"
+
+
+export const ModalView = styled.View`
+    bottom: 64px;
+    flex: 1;
+    align-items: center;
+    justify-content: flex-end;
+    margin-horizontal: 8px;
+`
+
+
+export const ModalBackground = styled.TouchableOpacity`
+    width: 100%;
+    border-radius: 2px;
+    background-color: rgba(0, 0, 0, 0.35);
+`
+
+
+export const ModalContent = styled.View`
+    align-items: center;
+    justify-content: center;
+`
