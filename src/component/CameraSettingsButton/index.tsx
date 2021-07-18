015f0e3d9baa8a4d309c3b2651de6b3d4185c3b0
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-07-18 10:08:22 -0300

    Add component CameraSettingsButton

---
+++ src/component/CameraSettingsButton/index.tsx
@@ -0,0 +1,24 @@
+import React from "react"
+import { TouchableOpacityProps } from "react-native"
+import Icon from "react-native-vector-icons/MaterialIcons"
+
+import { CameraSettingsButtonBase } from "./style"
+
+
+export interface CameraSettingsButtonProps extends TouchableOpacityProps {
+    icon: string,
+    size?: number,
+}
+
+
+export function CameraSettingsButton(props: CameraSettingsButtonProps) {
+    return (
+        <CameraSettingsButtonBase activeOpacity={0.6} {...props}>
+            <Icon
+                name={props.icon}
+                size={props.size || 24}
+                color={"rgb(255, 255, 255)"}
+            />
+        </CameraSettingsButtonBase>
+    )
+}
---
+++ src/component/CameraSettingsButton/style.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components/native"
+
+
+export const CameraSettingsButtonBase = styled.TouchableOpacity`
+    align-items: center;
+    justify-content: center;
+    width: 56px;
+    height: 56px;
+`
