4c3e05ba89b68b13320dc0a613b2c3116d286307
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-08-02 20:52:48 -0300

    Update ModalTitle with better style to Material Design

--- src/component/ModalComponent/ModalTitle.tsx
+++ src/component/ModalComponent/ModalTitle.tsx
@@ -6,8 +6,8 @@ import { styledProps } from "../../service/theme"
 export const ModalTitle = styled.Text`
     text-align-vertical: center;
     margin-horizontal: 24px;
-    height: 56px;
-    font-size: 18px;
+    height: 64px;
+    font-size: 20px;
     opacity: ${(props: styledProps) => props.theme.opacity.highEmphasis};
     color: ${(props: styledProps) => props.theme.color.modal_color};
 `
