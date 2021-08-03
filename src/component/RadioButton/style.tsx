5ea4e4fd8193951a3bf035b3b63414619f357485
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-08-02 23:08:08 -0300

    Update RadioButton height to better MaterialDesign

--- src/component/RadioButton/style.tsx
+++ src/component/RadioButton/style.tsx
@@ -8,8 +8,7 @@ export const Button = styled.TouchableOpacity`
     align-items: center;
     justify-content: flex-start;
     width: 100%;
-    height: 40px;
-    padding-vertical: 8px;
+    height: 48px;
 `
 
 
