b451342e7f04f01810774cccf216e7fc2d559ae6
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-12-25 13:14:20 -0300

    Reset temporarialy camera Control to before reanimated

--- src/screen/Camera/Control.tsx
+++ src/screen/Camera/Control.tsx
@@ -1,130 +1,35 @@
-import React, { ForwardRefRenderFunction, forwardRef, useImperativeHandle, useState, ForwardedRef } from "react"
-import { OrientationType, useDeviceOrientationChange } from "react-native-orientation-locker"
-import Reanimated, { useAnimatedStyle, useDerivedValue, useSharedValue, withTiming } from "react-native-reanimated"
+import React from "react"
 
 import { CameraControlAction, CameraControlButton, CameraControlView } from "../../component"
 
 
 export interface CameraControlProps {
-    screenAction?: "replace-picture";
-    pictureListLength: number;
-    addPictureFromGalery: () => void;
-    takePicture: () => void;
-    editDocument: () => void;
+    screenAction?: "replace-picture",
+    pictureListLength: number,
+    addPictureFromGalery: () => void,
+    takePicture: () => void,
+    editDocument: () => void,
 }
 
 
-export interface CameraControlHandle {
-    setTakePictureButtonEnable: (enable: boolean) => void;
-}
-
-
-const CameraControlBase: ForwardRefRenderFunction<CameraControlHandle, CameraControlProps> = (
-    props: CameraControlProps,
-    ref: ForwardedRef<CameraControlHandle>
-) => {
-
-
-    const rotationDegree = useSharedValue(0)
-
-    const [isTakePictureButtonDisable, setIsTakePictureButtonDisable] = useState(false)
-
-
-    useImperativeHandle(ref, () => ({
-        setTakePictureButtonEnable: (enable: boolean) => {
-            setIsTakePictureButtonDisable(!enable)
-        },
-    }))
-
-
-    useDeviceOrientationChange((newOrientation: OrientationType) => {
-        switch (newOrientation) {
-            case "PORTRAIT":
-                if (rotationDegree.value === 90) {
-                    rotationDegree.value -= 90
-                } else if (rotationDegree.value === 270) {
-                    rotationDegree.value += 90
-                } else {
-                    rotationDegree.value = 0
-                }
-                break
-            case "PORTRAIT-UPSIDEDOWN":
-                if (rotationDegree.value === 90) {
-                    rotationDegree.value += 90
-                } else if (rotationDegree.value === 270) {
-                    rotationDegree.value -= 90
-                } else {
-                    rotationDegree.value = 180
-                }
-                break
-            case "LANDSCAPE-LEFT":
-                if (rotationDegree.value === 0) {
-                    rotationDegree.value += 90
-                } else if (rotationDegree.value === 180) {
-                    rotationDegree.value -= 90
-                } else {
-                    rotationDegree.value = 90
-                }
-                break
-            case "LANDSCAPE-RIGHT":
-                if (rotationDegree.value === 0) {
-                    rotationDegree.value -= 90
-                } else if (rotationDegree.value === 180) {
-                    rotationDegree.value += 90
-                } else {
-                    rotationDegree.value = 270
-                }
-                break
-        }
-    })
-
-    const animatedRotation = useDerivedValue(() => {
-        return withTiming(rotationDegree.value, {
-            duration: 300,
-        })
-    })
-
-    const galleryStyle = useAnimatedStyle(() => {
-        return {
-            transform: [
-                { rotate: `${animatedRotation.value}deg` }
-            ]
-        }
-    })
-
-    const editDocumentStyle = useAnimatedStyle(() => {
-        return {
-            transform: [
-                { rotate: `${animatedRotation.value}deg` }
-            ]
-        }
-    })
-
-
+export function CameraControl(props: CameraControlProps) {
     return (
         <CameraControlView>
-            <Reanimated.View style={galleryStyle}>
-                <CameraControlButton
-                    icon={"collections"}
-                    onPress={props.addPictureFromGalery}
-                />
-            </Reanimated.View>
-
-
-            <CameraControlAction
-                disabled={isTakePictureButtonDisable}
-                onPress={props.takePicture}
+            <CameraControlButton
+                icon={"collections"}
+                onPress={props.addPictureFromGalery}
             />
 
 
+            <CameraControlAction onPress={props.takePicture} />
+
+
             {props?.screenAction !== "replace-picture" && (
-                <Reanimated.View style={editDocumentStyle}>
-                    <CameraControlButton
-                        icon={"description"}
-                        indexCount={props.pictureListLength.toString()}
-                        onPress={props.editDocument}
-                    />
-                </Reanimated.View>
+                <CameraControlButton
+                    icon={"description"}
+                    indexCount={props.pictureListLength.toString()}
+                    onPress={props.editDocument}
+                />
             )}
 
             {props?.screenAction === "replace-picture" && (
@@ -133,6 +38,3 @@ const CameraControlBase: ForwardRefRenderFunction<CameraControlHandle, CameraCon
         </CameraControlView>
     )
 }
-
-
-export const CameraControl = forwardRef(CameraControlBase)
