fc767fab4fd4a7eb90c69336aec65873bc3c51f6
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-12-25 15:09:43 -0300

    Update date to new react native version

--- src/service/date.tsx
+++ src/service/date.tsx
@@ -1,39 +1,40 @@
 
-const monthList = {
-    "Jan": "01", "Feb": "02", "Mar": "03",
-    "Apr": "04", "May": "05", "Jun": "06", 
-    "Jul": "07", "Aug": "08", "Sep": "09",
-    "Oct": "10", "Nov": "11", "Dec": "12"
-}
-
-
-export function getDate(currentDate=Date(), separator="/"): string {
-    const dateList = currentDate.split(" ")
-
-    const day = dateList[2]
-    const month = monthList[dateList[1]]
-    const year = dateList[3]
+export function getDate(separator = "/", currentDate = new Date()): string {
+    const day = currentDate.getDate().toString().padStart(2).replace(" ", "0")
+    const month = (currentDate.getMonth() + 1).toString().padStart(2).replace(" ", "0")
+    const year = currentDate.getFullYear().toString().padStart(2).replace(" ", "0")
 
     return `${day}${separator}${month}${separator}${year}`
 }
 
 
-export function getTime(currentDate=Date(), separator=":", hasSecond=false): string {
-    const timeList = currentDate.split(" ")[4].split(":")
+export function getTime(separator = ":", hasSecond = false, currentDate = new Date()): string {
+    const hour = currentDate.getHours().toString().padStart(2).replace(" ", "0")
+    const minute = currentDate.getMinutes().toString().padStart(2).replace(" ", "0")
 
-    const hour = timeList[0]
-    const minute = timeList[1]
-    const second = timeList[2]
+    let localeTime = `${hour}${separator}${minute}`
+    if (hasSecond) {
+        const second = currentDate.getSeconds().toString().padStart(2).replace(" ", "0")
+        localeTime += `${separator}${second}`
+    }
 
-    return `${hour}${separator}${minute}${hasSecond ? `${separator}${second}` : ""}`
+    return localeTime
 }
 
 
-export function getDateTime(dateSeparator="/", timeSeparator=":", hasSecond=false): string {
-    const currentDate = Date()
-
-    const date = getDate(currentDate, dateSeparator)
-    const time = getTime(currentDate, timeSeparator, hasSecond)
+export function getDateTime(dateSeparator = "/", timeSeparator = ":", hasSecond = false, currentDate = new Date()): string {
+    const date = getDate(dateSeparator, currentDate)
+    const time = getTime(timeSeparator, hasSecond, currentDate)
 
     return `${date} ${time}`
+}
+
+
+export function getTimestamp(): string {
+    return getDateTime("-", ":", true)
+}
+
+
+export function toDateTime(timestamp: string): string {
+    return getDateTime("/", ":", false, new Date(timestamp))
 }
