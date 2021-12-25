4a15c9ec8658811f4e620f93e99fbea9e79ab430
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-12-25 12:57:35 -0300

    Removed react-native-reanimated temporarialy as it is crashing

--- android/app/src/main/java/com/docscan/MainApplication.java
+++ android/app/src/main/java/com/docscan/MainApplication.java
@@ -7,9 +7,7 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.JSIModulePackage;
 import com.facebook.soloader.SoLoader;
-import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
@@ -33,11 +31,6 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
     @Override
     protected String getJSMainModuleName() {
       return "index";
-    }
-
-    @Override
-    protected JSIModulePackage getJSIModulePackage() {
-      return new ReanimatedJSIModulePackage();
     }
   };
 
