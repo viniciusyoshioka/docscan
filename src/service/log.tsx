a49fb302de46b6f6d7ad2fd0c2949d0689b9a804
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-12-25 15:23:46 -0300

    Remove current log functionality, but keep function to compatibility

--- src/service/log.tsx
+++ src/service/log.tsx
@@ -1,58 +1,7 @@
-import { Alert, ToastAndroid } from "react-native"
-import RNFS from "react-native-fs"
-
-import { fullPathLog } from "./constant"
-import { getReadWritePermission, ReadWritePermissionResul } from "./permission"
-
 
 export type logCode = "INFO" | "WARN" | "ERROR"
 
 
 export function log(code: logCode, data: string) {
-    if (!__DEV__) {
-        return
-    }
-
-    getReadWritePermission()
-        .then((logPermission: ReadWritePermissionResul) => {
-            if (!logPermission.READ_EXTERNAL_STORAGE || !logPermission.WRITE_EXTERNAL_STORAGE) {
-                ToastAndroid.show("Permissão negada para registrar logs", 10)
-                return false
-            }
-            return true
-        })
-        .then(async (hasPermission: boolean) => {
-            if (!hasPermission) {
-                return false
-            }
-            try {
-                const fileExists = await RNFS.exists(fullPathLog)
-                if (!fileExists) {
-                    await RNFS.writeFile(fullPathLog, "")
-                }
-                return true
-            } catch (error) {
-                console.log(`FALHA CRÍTICA - Erro criando arquivo de log. Mensagem: "${error}"`)
-                Alert.alert(
-                    "FALHA CRÍTICA",
-                    `Erro criando arquivo de log. Mensagem: "${error}"`
-                )
-                return false
-            }
-        })
-        .then(async (hasPermission: boolean) => {
-            if (!hasPermission) {
-                return false
-            }
-            console.log(`${code} - ${data}`)
-            try {
-                await RNFS.appendFile(fullPathLog, `${code} - ${data}\n`)
-            } catch (error) {
-                console.log(`FALHA CRÍTICA - Erro registrando log. Mensagem: "${error}"`)
-                Alert.alert(
-                    "FALHA CRÍTICA",
-                    `Erro registrando log. Mensagem: "${error}"`
-                )
-            }
-        })
+    return
 }
