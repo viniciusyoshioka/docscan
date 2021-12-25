3aaa9a09d71abbe014cbc5e74bcf45bb3e835839
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-12-25 17:13:17 -0300

    Add types for document

--- src/service/object-types.tsx
+++ src/service/object-types.tsx
@@ -1,13 +1,19 @@
 
 // Document
+export type DocumentForList = {
+    id: number,
+    name: string,
+    lastModificationTimestamp: string,
+}
+
+export type SimpleDocument = {
+    name: string,
+    pictureList: Array<string>,
+}
+
 export type Document = {
     id: number,
     name: string,
     pictureList: Array<string>,
-    lastModificationDate: string,
-}
-
-export type ExportedDocument = {
-    name: string,
-    pictureList: Array<string>,
+    lastModificationTimestamp: string,
 }
