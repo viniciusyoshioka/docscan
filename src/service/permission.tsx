4febe18ea32b5a02fd05e712fb15d27b4ea0da4a
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-12-25 15:26:15 -0300

    Improved permissions request

--- src/service/permission.tsx
+++ src/service/permission.tsx
@@ -1,68 +1,31 @@
 import { PermissionsAndroid } from "react-native"
 
 
-export type ReadWritePermissionResul = {
-    WRITE_EXTERNAL_STORAGE: boolean,
-    READ_EXTERNAL_STORAGE: boolean,
-}
-
-
 export async function getCameraPermission(): Promise<boolean> {
-    if (await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.CAMERA)) {
-        return true
+    const hasPermission = await PermissionsAndroid.check("android.permission.CAMERA")
+    if (!hasPermission) {
+        const requestedPermission = await PermissionsAndroid.request("android.permission.CAMERA")
+        return requestedPermission === "granted"
     }
-
-    const status = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.CAMERA)
-    if (status === "granted") {
-        return true
-    }
-    return false
-}
-
-
-export async function getReadWritePermission(): Promise<ReadWritePermissionResul> {
-    const hasWritePermission = await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE)
-    const hasReadPermission = await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE)
-    if (hasWritePermission && hasReadPermission) {
-        return {
-            WRITE_EXTERNAL_STORAGE: true,
-            READ_EXTERNAL_STORAGE: true
-        }
-    }
-
-    const status = await PermissionsAndroid.requestMultiple([
-        PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
-        PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE
-    ])
-
-    return {
-        WRITE_EXTERNAL_STORAGE: status["android.permission.WRITE_EXTERNAL_STORAGE"] === "granted",
-        READ_EXTERNAL_STORAGE: status["android.permission.READ_EXTERNAL_STORAGE"] === "granted"
-    }
+    return true
 }
 
 
 export async function getReadPermission(): Promise<boolean> {
-    if (await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE)) {
-        return true
+    const hasPermission = await PermissionsAndroid.check("android.permission.READ_EXTERNAL_STORAGE")
+    if (!hasPermission) {
+        const requestedPermission = await PermissionsAndroid.request("android.permission.READ_EXTERNAL_STORAGE")
+        return requestedPermission === "granted"
     }
-
-    const status = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE)
-    if (status === "granted") {
-        return true
-    }
-    return false
+    return true
 }
 
 
 export async function getWritePermission(): Promise<boolean> {
-    if (await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE)) {
-        return true
+    const hasPermission = await PermissionsAndroid.check("android.permission.WRITE_EXTERNAL_STORAGE")
+    if (!hasPermission) {
+        const requestedPermission = await PermissionsAndroid.request("android.permission.WRITE_EXTERNAL_STORAGE")
+        return requestedPermission === "granted"
     }
-
-    const status = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE)
-    if (status === "granted") {
-        return true
-    }
-    return false
+    return true
 }
