c7a7f012e8921eff19ad1f4835accddeca9dde18
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-12-25 15:38:17 -0300

    Add permission request to folder-handler

--- src/service/folder-handler.tsx
+++ src/service/folder-handler.tsx
@@ -1,86 +1,125 @@
 import RNFS from "react-native-fs"
 
 import { fullPathRoot, fullPathPdf, fullPathPicture, fullPathRootExternal, fullPathExported, fullPathTemporary, fullPathTemporaryExported, fullPathTemporaryCompressedPicture } from "./constant"
-import { log } from "./log"
+import { getWritePermission } from "./permission"
 
 
 async function createRootFolder() {
+    const hasWritePermission = await getWritePermission()
+    if (!hasWritePermission) {
+        console.log("Can't create root folder without write external storage permission")
+        return
+    }
+
     try {
-        if (!await RNFS.exists(fullPathRoot)) {
-            await RNFS.mkdir(fullPathRoot)
-        }
+        await RNFS.mkdir(fullPathRoot)
     } catch (error) {
-        log("ERROR", `folder-handler createRootFolder - Erro ao criar pasta. Mensagem: "${error}"`)
+        console.log(`folder-handler createRootFolder - Erro ao criar pasta. Mensagem: "${error}"`)
     }
 }
+
 
 async function createRootFolderExternal() {
+    const hasWritePermission = await getWritePermission()
+    if (!hasWritePermission) {
+        console.log("Can't create external root folder without write external storage permission")
+        return
+    }
+
     try {
-        if (!await RNFS.exists(fullPathRootExternal)) {
-            await RNFS.mkdir(fullPathRootExternal)
-        }
+        await RNFS.mkdir(fullPathRootExternal)
     } catch (error) {
-        log("ERROR", `folder-handler createRootFolderExternal - Erro ao criar pasta. Mensagem: "${error}"`)
+        console.log(`folder-handler createRootFolderExternal - Erro ao criar pasta. Mensagem: "${error}"`)
     }
 }
+
 
 async function createPdfFolder() {
+    const hasWritePermission = await getWritePermission()
+    if (!hasWritePermission) {
+        console.log("Can't create pdf folder without write external storage permission")
+        return
+    }
+
     try {
-        if (!await RNFS.exists(fullPathPdf)) {
-            await RNFS.mkdir(fullPathPdf)
-        }
+        await RNFS.mkdir(fullPathPdf)
     } catch (error) {
-        log("ERROR", `folder-handler createPdfFolder - Erro ao criar pasta. Mensagem: "${error}"`)
+        console.log(`folder-handler createPdfFolder - Erro ao criar pasta. Mensagem: "${error}"`)
     }
 }
+
 
 export async function createExportedFolder() {
+    const hasWritePermission = await getWritePermission()
+    if (!hasWritePermission) {
+        console.log("Can't create exported folder without write external storage permission")
+        return
+    }
+
     try {
-        if (!await RNFS.exists(fullPathExported)) {
-            await RNFS.mkdir(fullPathExported)
-        }
+        await RNFS.mkdir(fullPathExported)
     } catch (error) {
-        log("ERROR", `folder-handler createExportedFolder - Erro ao criar pasta. Mensagem: "${error}"`)
+        console.log(`folder-handler createExportedFolder - Erro ao criar pasta. Mensagem: "${error}"`)
     }
 }
+
 
 export async function createPictureFolder() {
+    const hasWritePermission = await getWritePermission()
+    if (!hasWritePermission) {
+        console.log("Can't create picture folder without write external storage permission")
+        return
+    }
+
     try {
-        if (!await RNFS.exists(fullPathPicture)) {
-            await RNFS.mkdir(fullPathPicture)
-        }
+        await RNFS.mkdir(fullPathPicture)
     } catch (error) {
-        log("ERROR", `folder-handler createPictureFolder - Erro ao criar pasta. Mensagem: "${error}"`)
+        console.log(`folder-handler createPictureFolder - Erro ao criar pasta. Mensagem: "${error}"`)
     }
 }
+
 
 export async function createTemporaryFolder() {
+    const hasWritePermission = await getWritePermission()
+    if (!hasWritePermission) {
+        console.log("Can't create temporary folder without write external storage permission")
+        return
+    }
+
     try {
-        if (!await RNFS.exists(fullPathTemporary)) {
-            await RNFS.mkdir(fullPathTemporary)
-        }
+        await RNFS.mkdir(fullPathTemporary)
     } catch (error) {
-        log("ERROR", `folder-handler createTemporaryFolder - Erro ao criar pasta. Mensagem: "${error}"`)
+        console.log(`folder-handler createTemporaryFolder - Erro ao criar pasta. Mensagem: "${error}"`)
     }
 }
+
 
 export async function createTemporaryExportedFolder() {
+    const hasWritePermission = await getWritePermission()
+    if (!hasWritePermission) {
+        console.log("Can't create temporary exported folder without write external storage permission")
+        return
+    }
+
     try {
-        if (!await RNFS.exists(fullPathTemporaryExported)) {
-            await RNFS.mkdir(fullPathTemporaryExported)
-        }
+        await RNFS.mkdir(fullPathTemporaryExported)
     } catch (error) {
-        log("ERROR", `folder-handler createTemporaryExportedFolder - Erro ao criar pasta. Mensagem: "${error}"`)
+        console.log(`folder-handler createTemporaryExportedFolder - Erro ao criar pasta. Mensagem: "${error}"`)
     }
 }
 
+
 export async function createTemporaryCompressedPictureFolder() {
+    const hasWritePermission = await getWritePermission()
+    if (!hasWritePermission) {
+        console.log("Can't create temporary compressed folder without write external storage permission")
+        return
+    }
+
     try {
-        if (!await RNFS.exists(fullPathTemporaryCompressedPicture)) {
-            await RNFS.mkdir(fullPathTemporaryCompressedPicture)
-        }
+        await RNFS.mkdir(fullPathTemporaryCompressedPicture)
     } catch (error) {
-        log("ERROR", `folder-handler createTemporaryCompressedPictureFolder - Erro ao criar pasta. Mensagem: "${error}"`)
+        console.log(`folder-handler createTemporaryCompressedPictureFolder - Erro ao criar pasta. Mensagem: "${error}"`)
     }
 }
 
