022b2228b92a28cac23ebebf7a90fd7d68892773
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-12-25 17:53:14 -0300

    Add default value for camera settings

---
+++ src/service/settings.ts
@@ -0,0 +1,11 @@
+import { cameraType, flashType, whiteBalanceType } from "../types"
+
+
+// Camera
+export const cameraFlashDefault: flashType = "off"
+
+export const cameraWhiteBalanceDefault: whiteBalanceType = "auto"
+
+export const cameraTypeDefault: cameraType = "back"
+
+export const cameraIdDefault = "0"
