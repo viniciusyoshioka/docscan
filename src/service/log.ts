256e5efc4fb895ee483bccb5c7229ca99ec2cde4
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-12-25 18:01:20 -0300

    Move log types to src/types

--- src/service/log.ts
+++ src/service/log.ts
@@ -3,71 +3,8 @@ import { Alert } from "react-native"
 import { logger } from "react-native-logs"
 
 import { LogDatabase } from "../database"
+import { Logger, transportFunctionProps } from "../types/log"
 import { getDateTime } from "./date"
-
-
-type transportFunctionProps = {
-    msg: string;
-    rawMsg: Array<string>;
-    level: {
-        severity: number;
-        text: string;
-    };
-    extension?: string | null;
-}
-
-
-type Logger = {
-    /**
-     * Disable an extension
-     */
-    disable: (extension: string) => boolean;
-
-    /**
-     * Enable an extension
-     */
-    enable: (extension: string) => boolean;
-
-    /**
-     * Extend logger with a new extension
-     */
-    extend: (extension: string) => (...msgs: any[]) => boolean | any;
-
-    /**
-     * Return all created extensions
-     */
-    getExtensions: () => string[];
-
-    /**
-     * Get current log severity API
-     */
-    getSeverity: () => string;
-
-    /**
-     * Set log severity API
-     */
-    setSeverity: (level: string) => string;
-
-    /**
-     * Log @param message as debug message
-     */
-    debug: (message: string) => void;
-
-    /**
-     * Log @param message as info message
-     */
-    info: (message: string) => void;
-
-    /**
-     * Log @param message as warn message
-     */
-    warn: (message: string) => void;
-
-    /**
-     * Log @param message as error message
-     */
-    error: (message: string) => void;
-}
 
 
 export function logCriticalError(message: string) {
---
+++ src/types/log.ts
@@ -0,0 +1,63 @@
+
+export type transportFunctionProps = {
+    msg: string;
+    rawMsg: Array<string>;
+    level: {
+        severity: number;
+        text: string;
+    };
+    extension?: string | null;
+}
+
+
+export type Logger = {
+    /**
+     * Disable an extension
+     */
+    disable: (extension: string) => boolean;
+
+    /**
+     * Enable an extension
+     */
+    enable: (extension: string) => boolean;
+
+    /**
+     * Extend logger with a new extension
+     */
+    extend: (extension: string) => (...msgs: any[]) => boolean | any;
+
+    /**
+     * Return all created extensions
+     */
+    getExtensions: () => string[];
+
+    /**
+     * Get current log severity API
+     */
+    getSeverity: () => string;
+
+    /**
+     * Set log severity API
+     */
+    setSeverity: (level: string) => string;
+
+    /**
+     * Log @param message as debug message
+     */
+    debug: (message: string) => void;
+
+    /**
+     * Log @param message as info message
+     */
+    info: (message: string) => void;
+
+    /**
+     * Log @param message as warn message
+     */
+    warn: (message: string) => void;
+
+    /**
+     * Log @param message as error message
+     */
+    error: (message: string) => void;
+}
