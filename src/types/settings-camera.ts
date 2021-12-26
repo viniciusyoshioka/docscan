e0ef6c54ca86498bc999d88cc404e5b6841e2a6d
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2021-12-26 14:37:27 -0300

    Add camera attributes in settings-camera type

--- src/types/settings-camera.ts
+++ src/types/settings-camera.ts
@@ -4,3 +4,11 @@ export type flashType = "auto" | "on" | "off"
 export type whiteBalanceType = "auto" | "sunny" | "cloudy" | "shadow" | "incandescent" | "fluorescent"
 
 export type cameraType = "front" | "back"
+
+
+export type CameraAttributes = {
+    flash: flashType,
+    whiteBalance: whiteBalanceType,
+    cameraType: cameraType,
+    cameraId: string,
+}
