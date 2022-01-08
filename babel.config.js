c9bbb211df7cc60e165e3f851fb42d6a0a304a06
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2022-01-07 23:48:32 -0300

    Add module resolver to tsconfig.json and babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-    presets: ["module:metro-react-native-babel-preset"]
+    presets: ["module:metro-react-native-babel-preset"],
+    plugins: [
+        [
+            "module-resolver",
+            {
+                root: ["./src"],
+                extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
+                alias: {
+                    "@components": "./src/components",
+                    "@database": "./src/database",
+                    "@image": "./src/image",
+                    "@screen": "./src/screen",
+                    "@services": "./src/services",
+                    "@type": "./src/types",
+                }
+            }
+        ]
+    ]
 }
