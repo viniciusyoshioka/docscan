0fe3e2005e4d90e1eb8a0644865744820db7c134
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2022-01-16 23:59:56 -0300

    Add type for camera orientation

---
+++ src/types/camera.ts
@@ -0,0 +1,8 @@
+
+/**
+ * VisionCamera orientation type
+ * 
+ * Created here because VisionCamera does not
+ * have a type for this props
+ */
+export type CameraOrientationType = "portrait" | "portraitUpsideDown" | "landscapeRight" | "landscapeLeft"
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
+export * from "./camera"
 export * from "./document"
 export * from "./log"
 export * from "./reducer-camera"
