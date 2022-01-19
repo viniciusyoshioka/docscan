9f1dc877179df3309fe2ba98b1d0048d53c7f85c
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2022-01-19 18:53:26 -0300

    Improved Modal code and update its design

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,41 +1,36 @@
-import React, { ReactChild, useCallback } from "react"
-import { Modal as RNModal, TouchableWithoutFeedback, ModalProps as RNModalProps, StyleProp, ViewStyle } from "react-native"
+import React from "react"
+import { Modal as RNModal, ModalProps as RNModalProps, StyleProp, ViewStyle } from "react-native"
 
-import { ModalBackground, ModalContent, ModalView } from "./style"
+import { ModalContent, ModalView } from "./style"
 
 
 export interface ModalProps extends RNModalProps {
-    visible: boolean,
-    setVisible: (visible: boolean) => void,
-    onClose?: () => void,
-    children?: ReactChild,
-    modalStyle?: StyleProp<ViewStyle>,
-    backgroundStyle?: StyleProp<ViewStyle>,
+    popupStyle?: StyleProp<ViewStyle>;
+    backgroundStyle?: StyleProp<ViewStyle>;
 }
 
 
 export function Modal(props: ModalProps) {
 
 
-    const closeModal = useCallback(() => {
-        if (props.onClose) {
-            props.onClose()
+    function closeModal() {
+        if (props.onRequestClose) {
+            props.onRequestClose()
         }
-        props.setVisible(false)
-    }, [])
+    }
 
 
     return (
         <RNModal transparent={true} visible={props.visible} onRequestClose={closeModal}>
-            <TouchableWithoutFeedback onPress={closeModal}>
-                <ModalView style={props.modalStyle}>
-                    <ModalBackground style={props.backgroundStyle} />
-
-                    <ModalContent activeOpacity={1}>
-                        {props?.children}
-                    </ModalContent>
-                </ModalView>
-            </TouchableWithoutFeedback>
+            <ModalView
+                activeOpacity={1}
+                onPress={closeModal}
+                style={props.backgroundStyle}
+            >
+                <ModalContent activeOpacity={1} style={props.popupStyle}>
+                    {props.children}
+                </ModalContent>
+            </ModalView>
         </RNModal>
     )
 }
--- src/components/Modal/style.tsx
+++ src/components/Modal/style.tsx
@@ -3,26 +3,17 @@ import styled from "styled-components/native"
 import { StyledProps } from "../../types"
 
 
-export const ModalView = styled.View`
+export const ModalView = styled.TouchableOpacity`
     align-items: center;
     justify-content: center;
     height: 100%;
-`
-
-
-export const ModalBackground = styled.View`
-    position: absolute;
-    top: 0px;
-    bottom: 0px;
-    left: 0px;
-    right: 0px;
-    background-color: rgba(0, 0, 0, 0.35);
+    background-color: rgba(0, 0, 0, 0.5);
 `
 
 
 export const ModalContent = styled.TouchableOpacity`
-    width: 280px;
-    elevation: 6;
-    border-radius: 1px;
+    width: 80%;
+    border-radius: 6px;
     background-color: ${(props: StyledProps) => props.theme.color.modal_background};
+    elevation: 6;
 `
