0b7437f21f12ca4773c60d2ddef166d500217e0d
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2022-04-15 14:12:39 -0300

    Add support for internationalization

---
+++ src/locales/index.ts
@@ -0,0 +1,53 @@
+import { Platform, NativeModules } from "react-native"
+import I18n from "i18n-js"
+
+import { en_US } from "./en_US"
+import { pt_BR } from "./pt_BR"
+
+
+const normalizeLanguageCode = {
+    "en": "en_US",
+    "en_US": "en_US",
+    "pt_BR": "pt_BR",
+    "pt_US": "pt_BR",
+}
+
+
+function getDeviceLanguage(): string {
+    switch (Platform.OS) {
+        case "android":
+            return normalizeLanguageCode[NativeModules.I18nManager.localeIdentifier]
+        case "ios":
+            return normalizeLanguageCode[NativeModules.SettingsManager.settings.AppleLocale]
+        default:
+            throw new Error("Only Android and iOS are supported.")
+    }
+}
+
+
+function setLanguageToI18n() {
+    const deviceLanguage = getDeviceLanguage()
+    const isLanguageSupported = I18n.translations.hasOwnProperty(deviceLanguage)
+    isLanguageSupported
+        ? I18n.locale = deviceLanguage
+        : I18n.defaultLocale = "en_US"
+}
+
+
+I18n.translations = {
+    "en_US": en_US,
+    "pt_BR": pt_BR,
+}
+
+setLanguageToI18n()
+
+
+/**
+ * Receives a key and returns the corresponding text
+ * already translated to device language, if available
+ * 
+ * @param key string key to get the translation in language object
+ * 
+ * @returns translated text
+ */
+export const translate = (key: string): string => I18n.t(key)
