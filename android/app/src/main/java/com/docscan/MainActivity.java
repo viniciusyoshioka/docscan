50393925001c192006ffa79ca14b784bb02d522c
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2022-07-05 20:32:20 -0300

    Fixed incorrect update in MainActivity.java

--- android/app/src/main/java/com/docscan/MainActivity.java
+++ android/app/src/main/java/com/docscan/MainActivity.java
@@ -26,13 +26,6 @@ public class MainActivity extends ReactActivity {
     return new MainActivityDelegate(this, getMainComponentName());
   }
 
-  @Override
-  protected boolean isConcurrentRootEnabled() {
-    // If you opted-in for the New Architecture, we enable Concurrent Root (i.e. React 18).
-    // More on this on https://reactjs.org/blog/2022/03/29/react-v18.html
-    return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
-  }
-
   public static class MainActivityDelegate extends ReactActivityDelegate {
     public MainActivityDelegate(ReactActivity activity, String mainComponentName) {
       super(activity, mainComponentName);
@@ -44,6 +37,13 @@ public class MainActivity extends ReactActivity {
       // If you opted-in for the New Architecture, we enable the Fabric Renderer.
       reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED);
       return reactRootView;
+    }
+
+    @Override
+    protected boolean isConcurrentRootEnabled() {
+      // If you opted-in for the New Architecture, we enable Concurrent Root (i.e. React 18).
+      // More on this on https://reactjs.org/blog/2022/03/29/react-v18.html
+      return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
     }
   }
 
