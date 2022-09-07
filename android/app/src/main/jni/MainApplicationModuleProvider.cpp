ef3904c7678cc669d9a10019492680970f4d392e
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2022-09-07 14:14:53 -0300

    Upgraded react-native to 0.70.0

--- android/app/src/main/jni/MainApplicationModuleProvider.cpp
+++ android/app/src/main/jni/MainApplicationModuleProvider.cpp
@@ -1,5 +1,6 @@
 #include "MainApplicationModuleProvider.h"
 
+#include <rncli.h>
 #include <rncore.h>
 
 namespace facebook
@@ -7,7 +8,7 @@ namespace facebook
     namespace react
     {
         std::shared_ptr<TurboModule> MainApplicationModuleProvider(
-            const std::string moduleName,
+            const std::string &moduleName,
             const JavaTurboModule::InitParams &params)
         {
             // Here you can provide your own module provider for TurboModules coming from
@@ -19,6 +20,14 @@ namespace facebook
             //    return module;
             // }
             // return rncore_ModuleProvider(moduleName, params);
+
+            // Module providers autolinked by RN CLI
+            auto rncli_module = rncli_ModuleProvider(moduleName, params);
+            if (rncli_module != nullptr)
+            {
+                return rncli_module;
+            }
+
             return rncore_ModuleProvider(moduleName, params);
         }
     } // namespace react
