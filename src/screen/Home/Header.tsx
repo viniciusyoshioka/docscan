f5a11ed48e65bda067cade8534ea901407f7fc13
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2022-10-01 15:01:03 -0300

    Removed React import from Home screen

--- src/screen/Home/Header.tsx
+++ src/screen/Home/Header.tsx
@@ -1,5 +1,3 @@
-import React from "react"
-
 import { Header, HeaderButton, HeaderTitle } from "../../components"
 import { appName } from "../../services/constant"
 import { HomeMenu } from "./HomeMenu"
