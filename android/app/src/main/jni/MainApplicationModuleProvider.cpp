6db5e56f217562e728e8aaf0be584cc5e0a74d94
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2022-10-10 22:27:24 -0300

    Downgraded react-native to 0.69.6

--- android/app/src/main/jni/MainApplicationModuleProvider.cpp
+++ android/app/src/main/jni/MainApplicationModuleProvider.cpp
@@ -8,7 +8,7 @@ namespace facebook
     namespace react
     {
         std::shared_ptr<TurboModule> MainApplicationModuleProvider(
-            const std::string &moduleName,
+            const std::string moduleName,
             const JavaTurboModule::InitParams &params)
         {
             // Here you can provide your own module provider for TurboModules coming from
@@ -20,13 +20,6 @@ namespace facebook
             //    return module;
             // }
             // return rncore_ModuleProvider(moduleName, params);
-
-            // Module providers autolinked by RN CLI
-            auto rncli_module = rncli_ModuleProvider(moduleName, params);
-            if (rncli_module != nullptr)
-            {
-                return rncli_module;
-            }
 
             return rncore_ModuleProvider(moduleName, params);
         }
