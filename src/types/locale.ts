76b5d1d5f1ad23d49c541538eabd4b542ea1ca85
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2022-10-16 20:50:58 -0300

    Added translation keys used in Home screen to TranslationKeyType

--- src/types/locale.ts
+++ src/types/locale.ts
@@ -3,7 +3,32 @@
  * Defines the keys used in translation object
  * and translation function param
  */
-export type TranslationKeyType = ""
+export type TranslationKeyType =
+    "cancel"
+    | "warn"
+    | "delete"
+
+    // Home alert
+    | "home_alert_errorLoadingDocuments_text"
+    | "home_alert_errorDeletingSelectedDocuments_text"
+    | "home_alert_deleteDocuments_title"
+    | "home_alert_deleteDocuments_text"
+    | "home_alert_exportingDocuments_title"
+    | "home_alert_exportingDocuments_text"
+    | "home_alert_noDocumentsToExport_text"
+    | "home_alert_exportDocuments_title"
+    | "home_alert_allSelectedDocumentsWillBeExported_text"
+    | "home_alert_allDocumentsWillBeExported_text"
+    | "home_alert_mergeDocuments_title"
+    | "home_alert_mergeDocuments_text"
+    | "home_alert_duplicateDocuments_title"
+    | "home_alert_duplicateDocuments_text"
+    // Home
+    | "home_export"
+    | "home_merge"
+    | "home_duplicate"
+    | "home_emptyDocumentList"
+    | "home_deletingDocuments"
 
 
 /**
