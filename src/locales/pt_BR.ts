e5be1c017cc49fd02d590354393407dd49999a78
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2022-10-16 20:51:21 -0300

    Add translation strings to pt_BR language

--- src/locales/pt_BR.ts
+++ src/locales/pt_BR.ts
@@ -3,5 +3,29 @@ import { TranslationType } from "../types"
 
 
 export const pt_BR: TranslationType = {
-    "": "",
+    cancel: "Cancelar",
+    warn: "Aviso",
+    delete: "Apagar",
+
+    // Home alert
+    home_alert_errorLoadingDocuments_text: "Erro ao carregar documentos",
+    home_alert_errorDeletingSelectedDocuments_text: "Erro ao apagar documentos selecionados",
+    home_alert_deleteDocuments_title: "Apagar",
+    home_alert_deleteDocuments_text: "Estes documentos serão apagados permanentemente",
+    home_alert_exportingDocuments_title: "Aguarde",
+    home_alert_exportingDocuments_text: "A exportação de documentos pode demorar um pouco",
+    home_alert_noDocumentsToExport_text: "Nenhum documento existente para ser exportado",
+    home_alert_exportDocuments_title: "Exportar",
+    home_alert_allSelectedDocumentsWillBeExported_text: "Os documentos selecionados serão exportados",
+    home_alert_allDocumentsWillBeExported_text: "Todos os documentos serão exportados",
+    home_alert_mergeDocuments_title: "Unir",
+    home_alert_mergeDocuments_text: "Os documento selecionados serão unidos em um único documento",
+    home_alert_duplicateDocuments_title: "Duplicar",
+    home_alert_duplicateDocuments_text: "Os documentos selecionados serão duplicados",
+    // Home
+    home_export: "Exportar",
+    home_merge: "Unir",
+    home_duplicate: "Duplicar",
+    home_emptyDocumentList: "Nenhum documento",
+    home_deletingDocuments: "Apagando documentos...",
 }
