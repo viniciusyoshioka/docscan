a5dedf69bb83c313e62ed7d9a23675efa4d5c353
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2022-10-23 12:51:58 -0300

    Updated getDate, getTime and getDateTime to return the date and time in the format YYYY-MM-DD and HH:MM:SS

--- src/services/date.ts
+++ src/services/date.ts
@@ -1,67 +1,47 @@
 
 /**
- * Get the date in format DD/MM/YYYY
+ * Get the date in format YYYY-MM-DD
  *
- * @param separator string to be used as separator
- * between day, month and year
- * @param currentDate Date object to extract the date from.
- * If not passed, a new one will be created to get latest date.
+ * @param dateObject Date object to extract the date from. Defaults to `new Date()`
  *
  * @returns string of the date
  */
-export function getDate(separator = "/", currentDate = new Date()): string {
-    const day = currentDate.getDate().toString().padStart(2, "0")
-    const month = (currentDate.getMonth() + 1).toString().padStart(2, "0")
-    const year = currentDate.getFullYear().toString().padStart(2, "0")
-
-    return `${day}${separator}${month}${separator}${year}`
+export function getDate(dateObject = new Date()): string {
+    const year = dateObject.getFullYear()
+    const month = (dateObject.getMonth() + 1).toString().padStart(2, "0")
+    const day = dateObject.getDate().toString().padStart(2, "0")
+    return `${year}-${month}-${day}`
 }
 
 
 /**
- * Get the time in format HH:MM or HH:MM:SS
+ * Get the time in format HH:MM:SS
  *
- * @param separator string to be used as separator
- * between hour, minute and seconds (if available)
- * @param hasSecond boolean indicating if seconds will
- * be added to time string
- * @param currentDate Date object to extract the time from.
- * If not passed, a new one will be created to get latest time.
+ * @param dateObject Date object to extract the time from. Defaults to `new Date()`
  *
  * @returns string of the time
  */
-export function getTime(separator = ":", hasSecond = false, currentDate = new Date()): string {
-    const hour = currentDate.getHours().toString().padStart(2, "0")
-    const minute = currentDate.getMinutes().toString().padStart(2, "0")
-
-    let localeTime = `${hour}${separator}${minute}`
-    if (hasSecond) {
-        const second = currentDate.getSeconds().toString().padStart(2, "0")
-        localeTime += `${separator}${second}`
-    }
+function getTime(dateObject = new Date()): string {
+    const hour = dateObject.getHours().toString().padStart(2, "0")
+    const minute = dateObject.getMinutes().toString().padStart(2, "0")
+    const second = dateObject.getSeconds().toString().padStart(2, "0")
+    return `${hour}:${minute}:${second}`
+}
 
     return localeTime
 }
 
 
 /**
- * Get the date time in format DD/MM/YYYY HH:MM or DD/MM/YYYY HH:MM:SS
+ * Get the date and time in format YYYY-MM-DD HH:MM:SS
  *
- * @param dateSeparator string to be used as separator
- * between day, month and year
- * @param timeSeparator string to be used as separator
- * between hour, minute and seconds (if available)
- * @param hasSecond boolean indicating if seconds will
- * be added to date time string
- * @param currentDate Date object to extract the date time from.
- * If not passed, a new one will be created to get latest date time.
+ * @param dateObject Date object to extract the date and time from. Defaults to `new Date()`
  *
- * @returns string of the date time
+ * @returns string of the date and time
  */
-export function getDateTime(dateSeparator = "/", timeSeparator = ":", hasSecond = false, currentDate = new Date()): string {
-    const date = getDate(dateSeparator, currentDate)
-    const time = getTime(timeSeparator, hasSecond, currentDate)
-
+export function getDateTime(dateObject = new Date()): string {
+    const date = getDate(dateObject)
+    const time = getTime(dateObject)
     return `${date} ${time}`
 }
 
@@ -94,5 +74,5 @@ export function getTimestamp(): string {
  * @returns string of the date time in format DD/MM/YYYY HH:MM
  */
 export function toDateTime(timestamp: string): string {
-    return getDateTime("/", ":", false, new Date(timestamp))
+    return getDateTime(new Date(timestamp))
 }
