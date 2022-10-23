63e6dd417af975adb66d51f8a8aeb130e7c665f3
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2022-10-23 15:21:41 -0300

    Updated getTimestamp to return ISO string and deleted toDateTime

--- src/services/date.ts
+++ src/services/date.ts
@@ -103,32 +103,12 @@ export function getLocaleDateTime(dateObject = new Date(), dateSeparator = "/", 
 
 
 /**
- * Get the date time, for timestamp, in format YYYY/MM/DD HH:MM:SS
+ * Get the date and time, for timestamp, in ISO format
+ *
+ * @param dateObject Date object to convert to ISO format. Defaults to `new Date()`
  *
  * @returns string of the timestamp
  */
-export function getTimestamp(): string {
-    const currentDate = new Date()
-
-    const day = currentDate.getDate().toString().padStart(2, "0")
-    const month = (currentDate.getMonth() + 1).toString().padStart(2, "0")
-    const year = currentDate.getFullYear().toString().padStart(2, "0")
-
-    const hour = currentDate.getHours().toString().padStart(2, "0")
-    const minute = currentDate.getMinutes().toString().padStart(2, "0")
-    const second = currentDate.getSeconds().toString().padStart(2, "0")
-
-    return `${year}/${month}/${day} ${hour}:${minute}:${second}`
-}
-
-
-/**
- * Get the date time string of the given timestamp
- *
- * @param timestamp string
- *
- * @returns string of the date time in format DD/MM/YYYY HH:MM
- */
-export function toDateTime(timestamp: string): string {
-    return getDateTime(new Date(timestamp))
+export function getTimestamp(dateObject = new Date()): string {
+    return dateObject.toISOString()
 }
