c2a114e5ad6a5e95cd4acfa0b9d07400c9af2f15
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2022-11-15 17:40:15 -0300

    Refactored index of database

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,16 +3,9 @@ import SQLite from "react-native-sqlite-storage"
 import { appDatabaseFullPath, logDatabaseFullPath } from "../services/constant"
 
 
-import * as DocumentDatabase from "./Document"
-export { DocumentDatabase }
-
-
-import * as LogDatabase from "./Log"
-export { LogDatabase }
-
-
-import * as SettingsDatabase from "./Settings"
-export { SettingsDatabase }
+export * as DocumentDatabase from "./Document"
+export * as LogDatabase from "./Log"
+export * as SettingsDatabase from "./Settings"
 
 
 /**
@@ -28,7 +21,7 @@ export let globalLogDatabase: SQLite.SQLiteDatabase
 
 /**
  * Sets the global app database
- * 
+ *
  * @param database the database instance
  * to be set as global app database
  */
@@ -39,7 +32,7 @@ export function setGlobalAppDatabase(database: SQLite.SQLiteDatabase) {
 
 /**
  * Sets the global log database
- * 
+ *
  * @param database the database instance
  * to be set as global log database
  */
@@ -50,64 +43,40 @@ export function setGlobalLogDatabase(database: SQLite.SQLiteDatabase) {
 
 /**
  * Opens app database
- * 
+ *
  * @returns the instance of the opened database
  */
-export function openAppDatabase(): Promise<SQLite.SQLiteDatabase> {
-    return new Promise((resolve, reject) => {
-        SQLite.openDatabase({
-            name: appDatabaseFullPath,
-            location: "default"
-        })
-            .then((db) => {
-                resolve(db)
-            })
-            .catch((error) => {
-                reject(error)
-            })
+export async function openAppDatabase(): Promise<SQLite.SQLiteDatabase> {
+    return await SQLite.openDatabase({
+        name: appDatabaseFullPath,
+        location: "default"
     })
 }
 
 
 /**
  * Opens log database
- * 
+ *
  * @returns the instance of the opened database
  */
-export function openLogDatabase(): Promise<SQLite.SQLiteDatabase> {
-    return new Promise((resolve, reject) => {
-        SQLite.openDatabase({
-            name: logDatabaseFullPath,
-            location: "default"
-        })
-            .then((db) => {
-                resolve(db)
-            })
-            .catch((error) => {
-                reject(error)
-            })
+export async function openLogDatabase(): Promise<SQLite.SQLiteDatabase> {
+    return await SQLite.openDatabase({
+        name: logDatabaseFullPath,
+        location: "default"
     })
 }
 
 
 /**
  * Opens a temporary database
- * 
+ *
  * @param databaseFilePath string of the name of the database file
- * 
+ *
  * @returns the instance of the opened database
  */
-export function openTemporaryDatabase(databaseFilePath: string): Promise<SQLite.SQLiteDatabase> {
-    return new Promise((resolve, reject) => {
-        SQLite.openDatabase({
-            name: databaseFilePath,
-            location: "default"
-        })
-            .then((db) => {
-                resolve(db)
-            })
-            .catch((error) => {
-                reject(error)
-            })
+export async function openTemporaryDatabase(databaseFilePath: string): Promise<SQLite.SQLiteDatabase> {
+    return await SQLite.openDatabase({
+        name: databaseFilePath,
+        location: "default"
     })
 }
