38a9363c686f2a62b8154f3709c2213f4cd84cf6
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2022-11-15 02:16:39 -0300

    Created components for sections of Camera screen

--- src/screen/Camera/style.ts
+++ src/screen/Camera/style.ts
@@ -1,20 +1,45 @@
+import { Text, View } from "react-native"
 import styled from "styled-components/native"
 
 import { StyledProps } from "../../types"
 
 
-export const CameraWrapper = styled.View`
+export const CameraTextWrapper = styled(View)`
     flex: 1;
-    align-items: center;
+    align-items: flex-start;
     justify-content: center;
-    width: 100%;
-    height: 100%;
+    padding: 16px;
 `
 
 
-export const NoCameraAvailableText = styled.Text`
-    font-size: 17px;
+export const CameraButtonWrapper = styled(View)`
+    align-items: center;
+    justify-content: center;
+    padding: 16px;
+`
+
+
+export const CameraWrapper = styled(View)`
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+`
+
+
+export const NoCameraAvailableTitle = styled(Text)`
+    width: 100%;
+    margin-bottom: 16px;
+    font-size: 19px;
+    font-weight: bold;
     text-align: center;
+    color: ${(props: StyledProps) => props.theme.color.screen_color};
+    opacity: ${(props: StyledProps) => props.theme.opacity.mediumEmphasis};
+`
+
+
+export const NoCameraAvailableText = styled(Text)`
+    margin-bottom: 2px;
+    font-size: 17px;
     color: ${(props: StyledProps) => props.theme.color.screen_color};
     opacity: ${(props: StyledProps) => props.theme.opacity.mediumEmphasis};
 `
