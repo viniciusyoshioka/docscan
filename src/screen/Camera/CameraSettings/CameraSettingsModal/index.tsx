ec65072793c2ee2f5a24889469968f25ae61a3d1
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2022-11-15 03:15:26 -0300

    Convert CameraSettingsModal to normal function

--- src/screen/Camera/CameraSettings/CameraSettingsModal/index.tsx
+++ src/screen/Camera/CameraSettings/CameraSettingsModal/index.tsx
@@ -12,7 +12,7 @@ export interface CameraSettingsModalProps {
 }
 
 
-export const CameraSettingsModal = (props: CameraSettingsModalProps) => {
+export function CameraSettingsModal(props: CameraSettingsModalProps) {
 
 
     function closeModal() {
