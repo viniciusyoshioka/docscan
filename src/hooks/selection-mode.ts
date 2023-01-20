b4a44a9279f0b06199e2d30c5d94ff0e3a7fee35
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-01-20 18:17:38 -0300

    Created hook for selectable items in selection-mode hook

--- src/hooks/selection-mode.ts
+++ src/hooks/selection-mode.ts
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { HandlerStateChangeEventPayload, LongPressGestureHandlerEventPayload, State } from "react-native-gesture-handler"
 
 
 export function useSelectionMode<T>() {
@@ -46,5 +47,43 @@ export function useSelectionMode<T>() {
         selectItem,
         deselectItem,
         exitSelection,
+    }
+}
+
+
+export interface SelectableItem {
+    onClick: () => void;
+    onSelect: () => void;
+    onDeselect: () => void;
+    isSelectionMode: boolean;
+    isSelected: boolean;
+}
+
+
+export function useSelectableItem<T extends SelectableItem>(props: T) {
+
+
+    function onPress() {
+        if (!props.isSelectionMode) {
+            props.onClick()
+        } else if (!props.isSelected) {
+            props.onSelect()
+        } else if (props.isSelected) {
+            props.onDeselect()
+        }
+    }
+
+    function onLongPress(nativeEvent: Readonly<HandlerStateChangeEventPayload & LongPressGestureHandlerEventPayload>) {
+        if (nativeEvent.state === State.ACTIVE) {
+            if (!props.isSelectionMode) {
+                props.onSelect()
+            }
+        }
+    }
+
+
+    return {
+        onPress,
+        onLongPress,
     }
 }
