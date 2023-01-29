a57b86b8bd111dd7ed3e00566e08a5b211b7efbd
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-01-29 02:50:33 -0300

    Refactor ImageVisualizationItem

--- src/screen/VisualizePicture/ImageVisualizationItem/index.tsx
+++ src/screen/VisualizePicture/ImageVisualizationItem/index.tsx
@@ -1,5 +1,5 @@
 import { ComponentClass, useEffect, useMemo, useState } from "react"
-import { Dimensions, Image, StatusBar, useWindowDimensions } from "react-native"
+import { Image, useWindowDimensions } from "react-native"
 import FastImage, { FastImageProps, Source } from "react-native-fast-image"
 import { Gesture, GestureDetector } from "react-native-gesture-handler"
 import Reanimated, {
@@ -16,6 +16,9 @@ import { Screen } from "../../../components"
 
 
 const AnimatedFastImage = Reanimated.createAnimatedComponent(FastImage as ComponentClass<FastImageProps>)
+
+
+const ANIMATION_DURATION = 150
 
 
 function clamp(value: number, lowerBound: number, upperBound: number): number {
@@ -41,24 +44,18 @@ export interface ImageVisualizationItemProps {
 export function ImageVisualizationItem(props: ImageVisualizationItemProps) {
 
 
-    const { width, height } = useWindowDimensions()
-
     const minZoom = useMemo(() => props.minZoom ?? 0.9, [props.minZoom])
     const maxZoom = useMemo(() => props.maxZoom ?? 10, [props.maxZoom])
     const doubleTabZoom = useMemo(() => props.doubleTabZoom ?? 2, [props.doubleTabZoom])
     const zoomMargin = useMemo(() => props.zoomMargin ?? 0, [props.zoomMargin])
     const allowZoomOut = useMemo(() => props.allowZoomOut ?? false, [props.allowZoomOut])
-    const windowWidth = useMemo(() => width, [width])
-    const windowHeight = useMemo(() => {
-        const { height: screenHeight } = Dimensions.get("screen")
-        const statusBarHeight = StatusBar.currentHeight ?? 0
 
-        if (screenHeight === height) {
-            return height - statusBarHeight
-        }
-        return height
-    }, [height, StatusBar.currentHeight])
-    const ANIMATION_DURATION = 150
+    const { width } = useWindowDimensions()
+
+    const [windowWidth, setWindowWidth] = useState(0)
+    const [windowHeight, setWindowHeight] = useState(0)
+    const [imageWidth, setImageWidth] = useState(0)
+    const [imageHeight, setImageHeight] = useState(0)
 
     const zoom = useSharedValue(1)
     const savedZoom = useSharedValue(1)
@@ -66,21 +63,19 @@ export function ImageVisualizationItem(props: ImageVisualizationItemProps) {
     const initialTranslationY = useSharedValue(0)
     const translateX = useSharedValue(0)
     const translateY = useSharedValue(0)
-    const imageWidth = useSharedValue(0)
-    const imageHeight = useSharedValue(0)
 
     const [isPanGestureEnabled, setIsPanGestureEnabled] = useState(false)
 
 
     const boudaries = useDerivedValue(() => {
         const margin = (zoom.value === 0) ? 0 : (zoomMargin / zoom.value)
-        const limitX = (imageWidth.value * zoom.value - windowWidth) / (2 * zoom.value)
-        const limitY = (imageHeight.value * zoom.value - windowHeight) / (2 * zoom.value)
+        const limitX = (imageWidth * zoom.value - windowWidth) / (2 * zoom.value)
+        const limitY = (imageHeight * zoom.value - windowHeight) / (2 * zoom.value)
         return {
             x: limitX + margin,
             y: limitY + margin,
         }
-    }, [zoomMargin, zoom.value, imageWidth.value, imageHeight.value, windowWidth, windowHeight])
+    }, [zoomMargin, zoom.value, imageWidth, imageHeight, windowWidth, windowHeight])
 
 
     const pinchGesture = Gesture.Pinch()
@@ -188,14 +183,14 @@ export function ImageVisualizationItem(props: ImageVisualizationItemProps) {
             initialTranslationY.value = translateY.value
         })
         .onChange(event => {
-            if (imageWidth.value * zoom.value > windowWidth) {
+            if (imageWidth * zoom.value > windowWidth) {
                 const newTranslateX = initialTranslationX.value + (event.translationX / zoom.value)
                 translateX.value = clamp(newTranslateX, -boudaries.value.x, boudaries.value.x)
             } else {
                 translateX.value = withTiming(0, { duration: ANIMATION_DURATION })
             }
 
-            if (imageHeight.value * zoom.value > windowHeight) {
+            if (imageHeight * zoom.value > windowHeight) {
                 const newTranslateY = initialTranslationY.value + (event.translationY / zoom.value)
                 translateY.value = clamp(newTranslateY, -boudaries.value.y, boudaries.value.y)
             } else {
@@ -244,14 +239,14 @@ export function ImageVisualizationItem(props: ImageVisualizationItemProps) {
                 newImageHeight = windowHeight
             }
 
-            imageWidth.value = newImageWidth
-            imageHeight.value = newImageHeight
+            setImageWidth(newImageWidth)
+            setImageHeight(newImageHeight)
         }
 
-        if (props.source.uri) {
+        if (props.source.uri && windowWidth !== 0 && windowHeight !== 0) {
             Image.getSize(props.source.uri, onSuccess, props.onError)
         }
-    }, [])
+    }, [windowWidth, windowHeight])
 
 
     return (
@@ -262,6 +257,10 @@ export function ImageVisualizationItem(props: ImageVisualizationItemProps) {
                     resizeMode={"contain"}
                     style={imageStyle}
                     onError={props.onError}
+                    onLayout={event => {
+                        setWindowWidth(event.nativeEvent.layout.width)
+                        setWindowHeight(event.nativeEvent.layout.height)
+                    }}
                 />
             </Screen>
         </GestureDetector>
