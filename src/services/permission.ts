299235377f594f951478d229dfda4d73d4594416
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-03-17 17:36:44 -0300

    Created function getReadMediaImagesPermission

--- src/services/permission.ts
+++ src/services/permission.ts
@@ -1,4 +1,4 @@
-import { PermissionsAndroid } from "react-native"
+import { Permission, PermissionsAndroid, Platform } from "react-native"
 
 // TODO request permission for Android and iOS
 
@@ -26,6 +26,25 @@ export async function getReadPermission(): Promise<boolean> {
     const hasPermission = await PermissionsAndroid.check("android.permission.READ_EXTERNAL_STORAGE")
     if (!hasPermission) {
         const requestedPermission = await PermissionsAndroid.request("android.permission.READ_EXTERNAL_STORAGE")
+        return requestedPermission === "granted"
+    }
+    return true
+}
+
+
+/**
+ * Request Android's `READ_MEDIA_IMAGES` permission to the user
+ *
+ * @returns boolean indicating if the permission was granted or not
+ */
+export async function getReadMediaImagesPermission(): Promise<boolean> {
+    const permission: Permission = Platform.Version >= 33
+        ? "android.permission.READ_MEDIA_IMAGES"
+        : "android.permission.READ_EXTERNAL_STORAGE"
+
+    const hasPermission = await PermissionsAndroid.check(permission)
+    if (!hasPermission) {
+        const requestedPermission = await PermissionsAndroid.request(permission)
         return requestedPermission === "granted"
     }
     return true
