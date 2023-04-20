37e0fab1b9fd45af54ba1a1963df0a819b5e7dc4
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-04-20 18:38:04 -0300

    Removed CAMERA_WHITE_BALANCE and CAMERA_ID from AppSettingsKeys as they arent used

--- src/services/storage/types.ts
+++ src/services/storage/types.ts
@@ -2,9 +2,7 @@
 export enum AppSettingsKeys {
     THEME = "settings.theme",
     CAMERA_FLASH = "settings.camera.flash",
-    CAMERA_WHITE_BALANCE = "settings.camera.white_balance",
     CAMERA_TYPE = "settings.camera.type",
-    CAMERA_ID = "settings.camera.id",
     CAMERA_RATIO = "settings.camera.ratio",
 }
 
