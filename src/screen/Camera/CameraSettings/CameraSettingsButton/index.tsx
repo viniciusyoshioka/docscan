80b13a55e6ef2bfca8b2f9c861dcab50f14d1df5
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-04-21 11:24:02 -0300

    Refactored CameraSettings

--- src/screen/Camera/CameraSettings/CameraSettingsButton/index.tsx
+++ src/screen/Camera/CameraSettings/CameraSettingsButton/index.tsx
@@ -1,35 +1,40 @@
+import { ExtendableIconProps, Icon, Text } from "@elementium/native"
 import { TouchableOpacityProps } from "react-native"
 
-import { Icon } from "@elementium/native"
-import { ButtonBase, ButtonChildrenWrapper, ButtonText } from "./style"
+import { CameraSettingsButtonBase } from "./style"
 
 
 export { CAMERA_SETTINGS_BUTTON_HEIGHT } from "./style"
 
 
-export interface CameraSettingsButtonProps extends TouchableOpacityProps {
+export interface CameraSettingsButtonProps extends TouchableOpacityProps, Omit<ExtendableIconProps, "style"> {
     optionName: string;
-    iconName: string;
-    iconSize?: number;
 }
 
 
 export function CameraSettingsButton(props: CameraSettingsButtonProps) {
-    return (
-        <ButtonBase activeOpacity={0.6} {...props}>
-            <ButtonChildrenWrapper>
-                <Icon
-                    name={props.iconName}
-                    size={props.iconSize || 24}
-                    color={"rgb(255, 255, 255)"}
-                />
-            </ButtonChildrenWrapper>
 
-            <ButtonChildrenWrapper>
-                <ButtonText numberOfLines={2}>
-                    {props.optionName}
-                </ButtonText>
-            </ButtonChildrenWrapper>
-        </ButtonBase>
+
+    const contentColor = props.iconColor ?? "white"
+
+
+    return (
+        <CameraSettingsButtonBase activeOpacity={0.6} {...props}>
+            <Icon
+                name={props.iconName}
+                group={props.iconGroup}
+                size={props.iconSize ?? 24}
+                color={contentColor}
+                style={{ flex: 1 }}
+            />
+
+            <Text
+                variant={"label"}
+                size={"medium"}
+                numberOfLines={2}
+                style={{ flex: 1, color: contentColor, textAlign: "center" }}
+                children={props.optionName}
+            />
+        </CameraSettingsButtonBase>
     )
 }
--- src/screen/Camera/CameraSettings/CameraSettingsButton/style.ts
+++ src/screen/Camera/CameraSettings/CameraSettingsButton/style.ts
@@ -4,24 +4,11 @@ import styled from "styled-components/native"
 
 export const CAMERA_SETTINGS_BUTTON_HEIGHT = 80
 
-export const ButtonBase = styled(TouchableOpacity)`
+
+export const CameraSettingsButtonBase = styled(TouchableOpacity)`
     align-items: center;
     justify-content: center;
     width: ${CAMERA_SETTINGS_BUTTON_HEIGHT}px;
     height: ${CAMERA_SETTINGS_BUTTON_HEIGHT}px;
     padding: 8px;
-`
-
-
-export const ButtonChildrenWrapper = styled.View`
-    flex: 1;
-    align-items: center;
-    justify-content: center;
-`
-
-
-export const ButtonText = styled.Text`
-    font-size: 13px;
-    text-align: center;
-    color: white;
 `
