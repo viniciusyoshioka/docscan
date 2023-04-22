43b120288bc3df527a0e116453309690166a401a
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-04-22 01:29:49 -0300

    Refactored pdf-creator service

--- src/services/pdf-creator.ts
+++ src/services/pdf-creator.ts
@@ -1,44 +1,20 @@
 import { NativeModules } from "react-native"
 
 
-const PdfCreator = NativeModules.PdfCreator
+const { PdfCreator } = NativeModules
 
 
-/**
- * Object containg options for PDF conversion
- */
 export type PdfCreatorOptions = {
-    /**
-     * Number from `0` to `100` indicating the
-     * compression level aplied to the images.
-     */
     imageCompressQuality: number;
-
-    /**
-     * Path to the temporary folder where images are
-     * stored until the conversion is over
-     */
     temporaryPath: string;
 }
 
 
-/**
- * Invokes a service to convert provided pictures to a PDF file
- *
- * @param pictureList string array of pictures paths to be converted
- * @param documentPath path to where the PDF file will be moved when the conversion is over
- * @param options `PdfCreatorOptions` object containing options for the conversion
- */
 export function createPdf(pictureList: string[], documentPath: string, options: PdfCreatorOptions) {
     PdfCreator.createPdf(pictureList, documentPath, options)
 }
 
 
-/**
- * Open the provided PDF with native document viewer
- *
- * @param filePath string with PDF file path
- */
 export function viewPdf(filePath: string) {
     PdfCreator.viewPdf(filePath)
 }
