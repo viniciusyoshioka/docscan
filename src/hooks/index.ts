07b273fec0a9d9fc66b807e8336bb333a24e1bbc
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-04-23 15:21:07 -0300

    Rename backhndler hook file to back-handler

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,5 @@
 export * from "./app-state"
-export * from "./backhandler"
+export * from "./back-handler"
 export * from "./camera-devices"
 export * from "./keep-awake"
 export * from "./keyboard"
