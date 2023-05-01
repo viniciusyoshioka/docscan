794fb425cb180443382d10779c6a91b98b809207
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-05-01 20:30:58 -0300

    Installed and configured realm and @realm/react

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,8 @@ module.exports = {
     presets: ["module:metro-react-native-babel-preset"],
     plugins: [
         "@babel/plugin-proposal-export-namespace-from",
+        ["@babel/plugin-proposal-decorators", { legacy: true } ],
+        "@realm/babel-plugin",
         "react-native-reanimated/plugin",
     ],
 }
