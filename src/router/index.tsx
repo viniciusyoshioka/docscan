481ea0ce934d7736fb0a326eca18941eea7cf03e
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-05-01 20:24:33 -0300

    Update animations of Router() to use the default values

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -20,11 +20,7 @@ export function Router() {
         <NavigationContainer>
             <Stack.Navigator
                 initialRouteName={"Home"}
-                screenOptions={{
-                    headerShown: false,
-                    animationTypeForReplace: "push",
-                    animation: "fade",
-                }}
+                screenOptions={{ headerShown: false }}
             >
                 <Stack.Screen name={"Home"} component={Home} />
                 <Stack.Screen name={"Camera"} component={Camera} options={{ orientation: "portrait" }} />
