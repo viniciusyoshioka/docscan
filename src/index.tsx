3e053fa481c3b433bfab71f4f5f541d761ebf6e6
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-05-01 20:42:35 -0300

    Removed implementation and usage of react-native-sqlite-storage

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,15 @@
-import { useEffect, useReducer, useState } from "react"
+import { useEffect, useReducer } from "react"
 import { useColorScheme } from "react-native"
 import { GestureHandlerRootView } from "react-native-gesture-handler"
 import { MenuProvider } from "react-native-popup-menu"
-import SQLite from "react-native-sqlite-storage"
 
 import { DocumentDatabase, LogDatabase, openAppDatabase, openLogDatabase, setGlobalAppDatabase, setGlobalLogDatabase } from "./database"
 import { useKeepAwakeOnDev } from "./hooks"
 import { Router } from "./router"
 import { DocumentDataProvider, reducerDocumentData } from "./services/document-data"
-import { logCriticalError, stringfyError } from "./services/log"
 import { Settings, settingsDefault, SettingsProvider } from "./services/settings"
 import { AppStorageKeys, setStorageDefaultValues, useMMKVObject } from "./services/storage"
 import { AppThemeProvider, themeDefault, ThemeType } from "./theme"
-
-
-SQLite.enablePromise(true)
 
 
 // TODO change database library
@@ -26,8 +21,6 @@ export function App() {
 
     const deviceTheme = useColorScheme()
 
-    const [appDb, setAppDb] = useState<SQLite.SQLiteDatabase>()
-    const [logDb, setLogDb] = useState<SQLite.SQLiteDatabase>()
     const [documentDataState, dispatchDocumentData] = useReducer(reducerDocumentData, undefined)
     const [settings, setSettings] = useMMKVObject<Settings>(AppStorageKeys.SETTINGS)
 
@@ -56,50 +49,10 @@ export function App() {
     useEffect(() => {
         const wasDefaultValuesSet = setStorageDefaultValues()
         if (wasDefaultValuesSet) setSettings(settingsDefault)
-
-        openAppDatabase()
-            .then(async database => {
-                function onTransactionError(error: SQLite.SQLError) {
-                    logCriticalError(`Error creating tables in app database: "${stringfyError(error)}"`)
-                }
-
-                async function onTransactionSuccess() {
-                    setAppDb(database)
-                }
-
-                setGlobalAppDatabase(database)
-
-                database.transaction(tx => {
-                    DocumentDatabase.createDocumentTable(tx)
-                }, onTransactionError, onTransactionSuccess)
-            })
-            .catch(error => {
-                logCriticalError(`Error opening app database: "${stringfyError(error)}"`)
-            })
-
-        openLogDatabase()
-            .then(async database => {
-                function onTransactionError(error: SQLite.SQLError) {
-                    logCriticalError(`Error creating tables in log database: "${stringfyError(error)}"`)
-                }
-
-                function onTransactionSuccess() {
-                    setLogDb(database)
-                }
-
-                setGlobalLogDatabase(database)
-
-                database.transaction(tx => {
-                    LogDatabase.createLogTable(tx)
-                }, onTransactionError, onTransactionSuccess)
-            })
-            .catch(error => {
-                logCriticalError(`Error opening log database: "${stringfyError(error)}"`)
-            })
     }, [])
 
 
-    if (!settings || !appDb || !logDb) {
+    if (!settings) {
         return null
     }
 
