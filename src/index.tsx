9c45271228443dcbc0ef38b184981cb7594f541c
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-05-20 13:49:41 -0300

    Moved theme loading to inside AppThemeProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,75 +1,32 @@
-import { useEffect, useReducer } from "react"
-import { useColorScheme } from "react-native"
 import { GestureHandlerRootView } from "react-native-gesture-handler"
 import { MenuProvider } from "react-native-popup-menu"
 
-import { RealmProvider } from "./database"
+import { DocumentModelProvider, RealmProvider } from "./database"
 import { useKeepAwakeOnDev } from "./hooks"
 import { Router } from "./router"
-import { DocumentDataProvider, reducerDocumentData } from "./services/document-data"
-import { Settings, settingsDefault, SettingsProvider } from "./services/settings"
-import { AppStorageKeys, setStorageDefaultValues, useMMKVObject } from "./services/storage"
-import { AppThemeProvider, themeDefault, ThemeType } from "./theme"
+import { SettingsProvider } from "./services/settings"
+import { AppThemeProvider } from "./theme"
 
 
-// TODO change database library
 export function App() {
 
 
     useKeepAwakeOnDev()
 
 
-    const deviceTheme = useColorScheme()
-
-    const [documentDataState, dispatchDocumentData] = useReducer(reducerDocumentData, undefined)
-    const [settings, setSettings] = useMMKVObject<Settings>(AppStorageKeys.SETTINGS)
-
-    const isDarkTheme = (settings?.theme === "auto" && deviceTheme === "dark") || settings?.theme === "dark"
-
-
-    function getAppTheme() {
-        const switchTheme = (newTheme: ThemeType) => {
-            if (settings) setSettings({ ...settings, theme: newTheme })
-        }
-
-        if (isDarkTheme) {
-            const { AppDarkTheme } = require("./theme")
-            AppDarkTheme.appTheme = settings?.theme
-            AppDarkTheme.switchTheme = switchTheme
-            return AppDarkTheme
-        }
-
-        const { AppLightTheme } = require("./theme")
-        AppLightTheme.appTheme = (settings?.theme ?? themeDefault) as ThemeType
-        AppLightTheme.switchTheme = switchTheme
-        return AppLightTheme
-    }
-
-
-    useEffect(() => {
-        const wasDefaultValuesSet = setStorageDefaultValues()
-        if (wasDefaultValuesSet) setSettings(settingsDefault)
-    }, [])
-
-
-    if (!settings) {
-        return null
-    }
-
-
     return (
         <GestureHandlerRootView style={{ flex: 1 }}>
-            <AppThemeProvider value={getAppTheme()}>
-                <MenuProvider backHandler={true}>
-                    <SettingsProvider value={{ settings, setSettings }}>
-                        <DocumentDataProvider value={{ documentDataState, dispatchDocumentData }}>
-                            <RealmProvider>
+            <RealmProvider>
+                <SettingsProvider>
+                    <DocumentModelProvider>
+                        <AppThemeProvider>
+                            <MenuProvider backHandler={true}>
                                 <Router />
-                            </RealmProvider>
-                        </DocumentDataProvider>
-                    </SettingsProvider>
-                </MenuProvider>
-            </AppThemeProvider>
+                            </MenuProvider>
+                        </AppThemeProvider>
+                    </DocumentModelProvider>
+                </SettingsProvider>
+            </RealmProvider>
         </GestureHandlerRootView>
     )
 }
--- src/theme/provider.tsx
+++ src/theme/provider.tsx
@@ -1,29 +1,65 @@
 import { ThemeProvider as ElementiumThemeProvider } from "@elementium/native"
-import { createContext, ProviderProps, useContext, useEffect } from "react"
-import { StatusBar, StatusBarStyle } from "react-native"
+import { createContext, ReactNode, useContext, useEffect } from "react"
+import { StatusBar, StatusBarStyle, useColorScheme } from "react-native"
 import { ThemeProvider as StyledThemeProvider } from "styled-components/native"
 
+import { Settings } from "../services/settings"
+import { AppStorageKeys, useMMKVObject } from "../services/storage"
+import { themeDefault } from "./constants"
 import { AppLightTheme } from "./light"
-import { AppThemeType } from "./types"
+import { AppThemeType, ThemeType } from "./types"
 
 
 const AppThemeContext = createContext<AppThemeType>(AppLightTheme)
 
 
-export function AppThemeProvider(props: ProviderProps<AppThemeType>) {
+export interface AppThemeProviderProps {
+    children?: ReactNode;
+}
+
+
+export function AppThemeProvider(props: AppThemeProviderProps) {
+
+
+    const deviceTheme = useColorScheme()
+
+    const [settings, setSettings] = useMMKVObject<Settings>(AppStorageKeys.SETTINGS)
+    const isDarkTheme = (settings?.theme === "auto" && deviceTheme === "dark") || settings?.theme === "dark"
+
+
+    function getAppTheme() {
+        const switchTheme = (newTheme: ThemeType) => {
+            if (settings) setSettings({ ...settings, theme: newTheme })
+        }
+
+        if (isDarkTheme) {
+            const { AppDarkTheme } = require("./dark")
+            AppDarkTheme.appTheme = settings?.theme
+            AppDarkTheme.switchTheme = switchTheme
+            return AppDarkTheme
+        }
+
+        const { AppLightTheme } = require("./light")
+        AppLightTheme.appTheme = (settings?.theme ?? themeDefault) as ThemeType
+        AppLightTheme.switchTheme = switchTheme
+        return AppLightTheme
+    }
+
+
+    const appTheme = getAppTheme()
 
 
     useEffect(() => {
-        const statusBarStyle: StatusBarStyle = props.value.isDark ? "light-content" : "dark-content"
+        const statusBarStyle: StatusBarStyle = appTheme.isDark ? "light-content" : "dark-content"
         StatusBar.setBarStyle(statusBarStyle)
-        StatusBar.setBackgroundColor(props.value.color.background)
-    }, [props.value.isDark, props.value.color.background])
+        StatusBar.setBackgroundColor(appTheme.color.background)
+    }, [appTheme.isDark, appTheme.color.background])
 
 
     return (
-        <AppThemeContext.Provider {...props}>
-            <ElementiumThemeProvider value={props.value}>
-                <StyledThemeProvider theme={props.value}>
+        <AppThemeContext.Provider value={appTheme}>
+            <ElementiumThemeProvider value={appTheme}>
+                <StyledThemeProvider theme={appTheme}>
                     {props.children}
                 </StyledThemeProvider>
             </ElementiumThemeProvider>
