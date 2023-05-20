b8eb72ec8369f78f17d4817f26e8abe2daecd867
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-05-20 13:50:48 -0300

    Delete setStorageDefaultValues from storage service

--- src/services/storage/mmkv.ts
+++ src/services/storage/mmkv.ts
@@ -1,16 +1,4 @@
 import { MMKV } from "react-native-mmkv"
 
-import { settingsDefault } from "../settings"
-import { AppStorageKeys } from "./types"
-
 
 export const storage = new MMKV()
-
-
-export function setStorageDefaultValues(): boolean {
-    const allKeys = storage.getAllKeys()
-    if (allKeys.length > 0) return false
-
-    storage.set(AppStorageKeys.SETTINGS, JSON.stringify(settingsDefault))
-    return true
-}
