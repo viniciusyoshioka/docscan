83d765b936c3b55009ae9076970dc2174acc4b0d
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-06-27 22:36:20 -0300

    Update RenameDocument to not use documentModel

--- src/screen/EditDocument/RenameDocument.tsx
+++ src/screen/EditDocument/RenameDocument.tsx
@@ -1,11 +1,14 @@
 import { Button, Modal, ModalActions, ModalProps, ModalTitle } from "@elementium/native"
+import { useRoute } from "@react-navigation/native"
 import { createRef, useEffect, useState } from "react"
 import { NativeSyntheticEvent, TextInput } from "react-native"
 
 import { Input } from "../../components"
-import { useDocumentModel, useDocumentRealm } from "../../database"
+import { DocumentSchema, useDocumentRealm } from "../../database"
 import { useKeyboard } from "../../hooks"
 import { translate } from "../../locales"
+import { DocumentService } from "../../services/document"
+import { RouteParamProps } from "../../types"
 
 
 export interface RenameDocumentProps extends ModalProps {}
@@ -14,12 +17,19 @@ export interface RenameDocumentProps extends ModalProps {}
 export function RenameDocument(props: RenameDocumentProps) {
 
 
+    const { params } = useRoute<RouteParamProps<"EditDocument">>()
+
     const documentRealm = useDocumentRealm()
-    const { documentModel, setDocumentModel } = useDocumentModel()
+    const documentId = params
+        ? Realm.BSON.ObjectID.createFromHexString(params.documentId)
+        : null
+    const document = documentId
+        ? documentRealm.objectForPrimaryKey<DocumentSchema>("DocumentSchema", documentId)
+        : null
 
     const inputRef = createRef<TextInput>()
 
-    const [documentName, setDocumentName] = useState<string | undefined>(undefined)
+    const [documentName, setDocumentName] = useState<string | undefined>(getDocumentName())
 
 
     useKeyboard("keyboardDidHide", () => {
@@ -27,22 +37,24 @@ export function RenameDocument(props: RenameDocumentProps) {
     })
 
 
+    function getDocumentName() {
+        return document ? document.name : DocumentService.getNewName()
+    }
+
     function renameDocument() {
         if (documentName === undefined) return
 
-        setDocumentModel({
-            type: "rename",
-            payload: {
-                realm: documentRealm,
-                newName: documentName,
-            },
+        documentRealm.write(() => {
+            if (document === null) return
+            // TODO create document if not exists and its renamed
+            document.name = documentName
         })
     }
 
 
     useEffect(() => {
         props.visible
-            ? setDocumentName(documentModel?.name)
+            ? setDocumentName(getDocumentName())
             : setDocumentName(undefined)
     }, [props.visible])
 
