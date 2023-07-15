a379661d955633ef3ed813a06f5798b74d5dd419
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-07-15 20:50:07 -0300

    Refactor constant.ts to remove excessive and unused constants

--- src/services/constant.ts
+++ src/services/constant.ts
@@ -7,53 +7,26 @@ export const appName = "DocScan"
 export const appVersion = "1.18.0"
 export const appType = __DEV__ ? "beta" : "release"
 
-// Icon
+// Icons
 export const appIcon = require("./../image/docscan.png")
 export const appIconOutline = require("./../image/docscan_outline.png")
 
 // Folder
-// Root
+// Root folders
 export const folderRoot = appName
-export const fullPathRoot = `${RNFS.DocumentDirectoryPath}/${folderRoot}`
+export const fullPathRootInternal = `${RNFS.DocumentDirectoryPath}/${folderRoot}`
 export const fullPathRootExternal = `${RNFS.ExternalStorageDirectoryPath}/Android/media/${appId}/${folderRoot}`
-// External/Export
-export const folderExported = "Exported"
-export const relativePathExported = `${folderRoot}/${folderExported}`
-export const fullPathExported = `${fullPathRootExternal}/${folderExported}`
-// External/PDF
-export const folderPdf = "PDF"
-export const relativePathPdf = `${folderRoot}/${folderPdf}`
-export const fullPathPdf = `${fullPathRootExternal}/${folderPdf}`
-// Internal/Picture
-export const folderPicture = "Picture"
-export const relativePathPicture = `${folderRoot}/${folderPicture}`
-export const fullPathPicture = `${fullPathRoot}/${folderPicture}`
-// Internal/Temporary
-export const folderTemporary = "Temporary"
-export const relativePathTemporary = `${folderRoot}/${folderTemporary}`
-export const fullPathTemporary = `${fullPathRoot}/${folderTemporary}`
-// Internal/Temporary/Exported
-export const folderTemporaryExported = "Exported"
-export const relativePathTemporaryExported = `${relativePathTemporary}/${folderTemporaryExported}`
-export const fullPathTemporaryExported = `${fullPathTemporary}/${folderTemporaryExported}`
-// Internal/Temporary/Imported
-export const folderTemporaryImported = "Imported"
-export const relativePathTemporaryImported = `${relativePathTemporary}/${folderTemporaryImported}`
-export const fullPathTemporaryImported = `${fullPathTemporary}/${folderTemporaryImported}`
-// Internal/Temporary/CompressedPicture
-export const folderTemporaryCompressedPicture = "CompressedPicture"
-export const relativePathTemporaryCompressedPicture = `${relativePathTemporary}/${folderTemporaryCompressedPicture}`
-export const fullPathTemporaryCompressedPicture = `${fullPathTemporary}/${folderTemporaryCompressedPicture}`
+// External folders
+export const fullPathExported = `${fullPathRootExternal}/Exported`
+export const fullPathPdf = `${fullPathRootExternal}/PDF`
+// Internal folders
+export const fullPathPicture = `${fullPathRootInternal}/Picture`
+export const fullPathTemporaryExported = `${fullPathRootInternal}/TemporaryExported`
+export const fullPathTemporaryImported = `${fullPathRootInternal}/TemporaryImported`
+export const fullPathTemporaryCompressedPicture = `${fullPathRootInternal}/TemporaryCompressedPicture`
 
-// Database
-// Database folder
+// Database folders
 export const databaseFolder = RNFS.DocumentDirectoryPath
-// App database
-export const appDatabaseFileName = "docscan_database.realm"
-export const appDatabaseFullPath = `${databaseFolder}/${appDatabaseFileName}`
-// Log database
-export const logDatabaseFileName = "docscan_log.realm"
-export const logDatabaseFullPath = `${databaseFolder}/${logDatabaseFileName}`
-// Export database
-export const exportDatabaseFileName = "docscan_export.realm"
-export const exportDatabaseFullPath = `${databaseFolder}/${exportDatabaseFileName}`
+export const appDatabaseFullPath = `${databaseFolder}/docscan_database.realm`
+export const logDatabaseFullPath = `${databaseFolder}/docscan_log.realm`
+export const exportDatabaseFullPath = `${databaseFolder}/docscan_export.realm`
