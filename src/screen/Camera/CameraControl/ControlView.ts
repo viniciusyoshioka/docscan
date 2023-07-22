125fa18ed768890fac448dc13602ce433d8a3ad2
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-07-22 14:46:31 -0300

    Changed CONTROL_VIEW_MIN_HEIGHT value to window height calculus

--- src/screen/Camera/CameraControl/ControlView.ts
+++ src/screen/Camera/CameraControl/ControlView.ts
@@ -1,8 +1,18 @@
+import { Dimensions } from "react-native"
 import styled from "styled-components/native"
+
+import { HEADER_HEIGHT } from "../../../components"
+import { getCameraSize } from "../utils"
+
+
+const { width, height } = Dimensions.get("window")
+const cameraSize = getCameraSize({ width, height }, "3:4")
 
 
 export const CONTROL_VIEW_PADDING_VERTIVAL = 16
-export const CONTROL_VIEW_MIN_HEIGHT = 56
+export const CONTROL_VIEW_MIN_HEIGHT = cameraSize
+    ? height - HEADER_HEIGHT - cameraSize.height
+    : 2 * CONTROL_VIEW_PADDING_VERTIVAL
 
 
 export interface ControlViewProps {
