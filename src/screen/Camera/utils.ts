0e516215c7218644d125ef947874aa2a84726dbb
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-07-22 14:42:36 -0300

    Created getCameraSize util

---
+++ src/screen/Camera/utils.ts
@@ -0,0 +1,21 @@
+import { CameraRatio, getCameraRatioNumber } from "../../services/settings"
+
+
+export type ScreenSize = {
+    width: number;
+    height: number;
+}
+
+export type CameraSize = {
+    width: number;
+    height: number;
+}
+
+export function getCameraSize(screenSize: ScreenSize, cameraRatio: CameraRatio): CameraSize | undefined {
+    const cameraWidth = screenSize.width
+    const cameraHeight = screenSize.width * getCameraRatioNumber(cameraRatio)
+    if (cameraHeight > screenSize.height) {
+        return undefined
+    }
+    return { width: cameraWidth, height: cameraHeight }
+}
