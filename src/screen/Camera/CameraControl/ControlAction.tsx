420240ff2ae34bd671949c207d3ee443e72dc223
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-07-22 22:52:32 -0300

    Add animation in ControlAction when pressed

--- src/screen/Camera/CameraControl/ControlAction.tsx
+++ src/screen/Camera/CameraControl/ControlAction.tsx
@@ -1,8 +1,12 @@
 import { Color } from "@elementium/color"
 import { useMemo } from "react"
-import { StyleSheet, TouchableOpacity, TouchableOpacityProps } from "react-native"
+import { GestureResponderEvent, StyleSheet, TouchableOpacity, TouchableOpacityProps } from "react-native"
+import Reanimated, { useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated"
 
 import { useAppTheme } from "../../../theme"
+
+
+const ReanimatedTouchableOpacity = Reanimated.createAnimatedComponent(TouchableOpacity)
 
 
 export const CONTROL_ACTION_SIZE = 64
@@ -17,6 +21,26 @@ export function ControlAction(props: ControlActionProps) {
 
 
     const { color, state } = useAppTheme()
+
+    const isPressed = useSharedValue(false)
+
+
+    function onPressIn(e: GestureResponderEvent) {
+        isPressed.value = true
+        if (props.onPressIn) props.onPressIn(e)
+    }
+
+    function onPressOut(e: GestureResponderEvent) {
+        isPressed.value = false
+        if (props.onPressOut) props.onPressOut(e)
+    }
+
+
+    const animatedScale = useAnimatedStyle(() => ({
+        transform: [
+            { scale: withTiming(isPressed.value ? 0.9 : 1, { duration: 100 }) }
+        ]
+    }), [isPressed])
 
 
     const style = StyleSheet.flatten(props.style)
@@ -34,10 +58,12 @@ export function ControlAction(props: ControlActionProps) {
 
 
     return (
-        <TouchableOpacity
-            activeOpacity={0.7}
+        <ReanimatedTouchableOpacity
+            activeOpacity={1}
             {...props}
-            style={[styles.container, props.style, { backgroundColor } ]}
+            onPressIn={onPressIn}
+            onPressOut={onPressOut}
+            style={[styles.container, props.style, { backgroundColor }, animatedScale]}
         />
     )
 }
