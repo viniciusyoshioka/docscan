77c1ecbbfbc3215d5caa0f30bec8709c688d8c22
--global <--global>
2023-08-13 00:59:35 -0300

    Update Router to made StatusBar translucent and transparent in all screens

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,5 +1,5 @@
 import { NavigationContainer } from "@react-navigation/native"
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { createNativeStackNavigator, NativeStackNavigationOptions } from "@react-navigation/native-stack"
 import { enableScreens } from "react-native-screens"
 
 import { Camera } from "../screen/Camera"
@@ -19,21 +19,31 @@ const Stack = createNativeStackNavigator<ScreenParams>()
 
 
 export function Router() {
+
+
+    const stackNavigatorScreenOptions: NativeStackNavigationOptions = {
+        animation: "fade",
+        headerShown: false,
+        statusBarColor: "transparent",
+        statusBarTranslucent: true,
+    }
+
+
     return (
         <NavigationContainer>
-            <Stack.Navigator
-                initialRouteName={"Home"}
-                screenOptions={{ headerShown: false, animation: "fade" }}
-            >
+            <Stack.Navigator initialRouteName={"Home"} screenOptions={stackNavigatorScreenOptions}>
                 <Stack.Screen name={"Home"} component={Home} />
                 <Stack.Screen name={"Camera"} component={Camera} options={{ orientation: "portrait" }} />
                 <Stack.Screen name={"Settings"} component={Settings} />
-                <Stack.Screen name={"ChangeTheme"} component={ChangeTheme} options={{ presentation: "transparentModal" }} />
                 <Stack.Screen name={"EditDocument"} component={EditDocument} />
-                <Stack.Screen name={"ConvertPdfOption"} component={ConvertPdfOption} options={{ presentation: "transparentModal" }} />
-                <Stack.Screen name={"RenameDocument"} component={RenameDocument} options={{ presentation: "transparentModal" }} />
                 <Stack.Screen name={"VisualizePicture"} component={VisualizePicture} />
                 <Stack.Screen name={"Gallery"} component={Gallery} />
+
+                <Stack.Group screenOptions={{ presentation: "transparentModal" }}>
+                    <Stack.Screen name={"ChangeTheme"} component={ChangeTheme} />
+                    <Stack.Screen name={"ConvertPdfOption"} component={ConvertPdfOption} />
+                    <Stack.Screen name={"RenameDocument"} component={RenameDocument} />
+                </Stack.Group>
             </Stack.Navigator>
         </NavigationContainer>
     )
