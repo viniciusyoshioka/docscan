560c2ab675dd7e9ed40f0a2b65a382a7ab59246d
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-10-07 23:24:02 -0300

    Add safe area insets in SettingsHeader

--- src/screen/Settings/Header.tsx
+++ src/screen/Settings/Header.tsx
@@ -1,6 +1,6 @@
 import { useNavigation } from "@react-navigation/native"
-import { StatusBar } from "react-native"
 import { Appbar } from "react-native-paper"
+import { useSafeAreaInsets } from "react-native-safe-area-context"
 
 import { translate } from "@locales"
 import { NavigationParamProps } from "@router"
@@ -12,11 +12,13 @@ export interface SettingsHeaderProps {}
 export function SettingsHeader(props: SettingsHeaderProps) {
 
 
+    const safeAreaInsets = useSafeAreaInsets()
+
     const navigation = useNavigation<NavigationParamProps<"Settings">>()
 
 
     return (
-        <Appbar.Header elevated={true} statusBarHeight={StatusBar.currentHeight}>
+        <Appbar.Header elevated={true} statusBarHeight={safeAreaInsets.top}>
             <Appbar.BackAction onPress={() => navigation.navigate("Home")} />
 
             <Appbar.Content title={translate("Settings_header_title")} />
