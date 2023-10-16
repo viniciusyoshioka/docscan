4b6159419a14d4d9c36971d7b176ddde88c359fb
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-10-16 13:07:59 -0300

    Deleted custom useCameraDevices

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,5 @@
 export * from "./app-state"
 export * from "./back-handler"
-export * from "./camera-devices"
 export * from "./keep-awake"
 export * from "./keyboard"
 export * from "./orientation"
