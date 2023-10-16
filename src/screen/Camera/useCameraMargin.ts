1013f03d36831645dfc37ad5217674421c3a2b84
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-10-16 15:30:55 -0300

    Revert useCameraMargin

--- src/screen/Camera/useCameraMargin.ts
+++ src/screen/Camera/useCameraMargin.ts
@@ -1,10 +1,28 @@
+import { useWindowDimensions } from "react-native"
+import { useSafeAreaInsets } from "react-native-safe-area-context"
+
+import { useSettings } from "@services/settings"
+import { HEADER_HEIGHT } from "./Header"
+import { getCameraSize } from "./utils"
 
 
 export type CameraMargin = {
-    top: number;
+    top: number
 }
 
 
 export function useCameraMargin(): CameraMargin {
+
+
+    const safeAreaInsets = useSafeAreaInsets()
+    const { width, height } = useWindowDimensions()
+
+    const { settings } = useSettings()
+
+
+    const defaultCameraSize = getCameraSize({ width, height }, settings.camera.ratio)
+    if ((defaultCameraSize.height + HEADER_HEIGHT + safeAreaInsets.top) < height) {
+        return { top: HEADER_HEIGHT + safeAreaInsets.top }
+    }
     return { top: 0 }
 }
