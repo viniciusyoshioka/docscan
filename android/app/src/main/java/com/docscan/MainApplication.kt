ed7a02ebdc60b34ec310c8142ba7a23a0b80d6e3
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-01-10 20:58:46 -0300

    Upgrade react native to 0.73.2

---
+++ android/app/src/main/java/com/docscan/MainApplication.kt
@@ -0,0 +1,58 @@
+package com.docscan
+
+import android.app.Application
+import cl.json.ShareApplication
+import com.docscan.DocumentService.DocumentServicePackage
+import com.docscan.ImageCrop.ImageCropPackage
+import com.docscan.ImageTools.ImageToolsPackage
+import com.docscan.PdfCreator.PdfCreatorPackage
+import com.facebook.react.PackageList
+import com.facebook.react.ReactApplication
+import com.facebook.react.ReactHost
+import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
+import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.react.flipper.ReactNativeFlipper
+import com.facebook.soloader.SoLoader
+
+class MainApplication : Application(), ReactApplication, ShareApplication {
+
+  override val reactNativeHost: ReactNativeHost =
+      object : DefaultReactNativeHost(this) {
+        override fun getPackages(): List<ReactPackage> =
+            PackageList(this).packages.apply {
+              // Packages that cannot be autolinked yet can be added manually here, for example:
+              // add(MyReactNativePackage())
+              add(DocumentServicePackage())
+              add(ImageCropPackage())
+              add(ImageToolsPackage())
+              add(PdfCreatorPackage())
+            }
+
+        override fun getJSMainModuleName(): String = "index"
+
+        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+
+        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+      }
+
+  override val reactHost: ReactHost
+    get() = getDefaultReactHost(this.applicationContext, reactNativeHost)
+
+  override fun onCreate() {
+    super.onCreate()
+    SoLoader.init(this, false)
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      // If you opted-in for the New Architecture, we load the native entry point for this app.
+      load()
+    }
+    ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
+  }
+
+  override fun getFileProviderAuthority(): String {
+    return BuildConfig.APPLICATION_ID + ".provider"
+  }
+}
