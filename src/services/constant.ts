ba644d7171146a8636e3ff89b60b07d64b53134d
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-01-20 19:57:02 -0300

    Update app version

--- src/services/constant.ts
+++ src/services/constant.ts
@@ -6,7 +6,7 @@ export class Constants {
     // App
     static appId = "com.docscan"
     static appName = "DocScan"
-    static appVersion = "1.19.3"
+    static appVersion = "1.19.4"
     static appType = __DEV__ ? "dev" : "release"
 
     // Icons
