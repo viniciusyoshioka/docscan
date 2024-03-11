cb57442f1787b712d7b393f8a5baa829162b8fab
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-03-11 18:07:21 -0300

    update: Remove elementium usage from ChangeTheme

--- src/screen/Settings/ChangeTheme.tsx
+++ src/screen/Settings/ChangeTheme.tsx
@@ -1,9 +1,7 @@
-import { Modal } from "@elementium/native"
 import { useNavigation } from "@react-navigation/native"
 import { useState } from "react"
-import { Button, RadioButton } from "react-native-paper"
+import { Button, Dialog, RadioButton } from "react-native-paper"
 
-import { useBackHandler } from "@hooks"
 import { translate } from "@locales"
 import { NavigationParamProps } from "@router"
 import { ThemeType, useAppTheme } from "@theme"
@@ -13,70 +11,56 @@ export function ChangeTheme() {
 
 
     const navigation = useNavigation<NavigationParamProps<"ChangeTheme">>()
-
     const { appTheme, switchTheme } = useAppTheme()
 
     const [selectedTheme, setSelectedTheme] = useState(appTheme)
 
 
-    useBackHandler(() => goBack())
-
-
-    function goBack() {
-        navigation.goBack()
-        return true
-    }
-
-
     return (
-        <Modal.Scrim onPress={goBack}>
-            <Modal.Container >
-                <Modal.Title>
-                    {translate("ChangeTheme_title")}
-                </Modal.Title>
+        <Dialog visible={true} onDismiss={navigation.goBack}>
+            <Dialog.Title>
+                {translate("ChangeTheme_title")}
+            </Dialog.Title>
 
-                <Modal.Content hasDivider={false}>
-                    <RadioButton.Group
-                        value={selectedTheme}
-                        onValueChange={newValue => setSelectedTheme(newValue as ThemeType)}
-                    >
-                        <RadioButton.Item
-                            label={translate("ChangeTheme_auto")}
-                            value={"auto"}
-                            style={{ paddingLeft: 24 }}
-                        />
-
-                        <RadioButton.Item
-                            label={translate("ChangeTheme_light")}
-                            value={"light"}
-                            style={{ paddingLeft: 24 }}
-                        />
-
-                        <RadioButton.Item
-                            label={translate("ChangeTheme_dark")}
-                            value={"dark"}
-                            style={{ paddingLeft: 24 }}
-                        />
-                    </RadioButton.Group>
-                </Modal.Content>
-
-                <Modal.Actions>
-                    <Button
-                        mode={"text"}
-                        children={translate("cancel")}
-                        onPress={goBack}
+            <Dialog.Content>
+                <RadioButton.Group
+                    value={selectedTheme}
+                    onValueChange={newValue => setSelectedTheme(newValue as ThemeType)}
+                >
+                    <RadioButton.Item
+                        label={translate("ChangeTheme_auto")}
+                        value={"auto"}
+                        style={{ paddingHorizontal: 0 }}
                     />
 
-                    <Button
-                        mode={"text"}
-                        children={translate("ok")}
-                        onPress={() => {
-                            switchTheme(selectedTheme)
-                            goBack()
-                        }}
+                    <RadioButton.Item
+                        label={translate("ChangeTheme_light")}
+                        value={"light"}
+                        style={{ paddingHorizontal: 0 }}
                     />
-                </Modal.Actions>
-            </Modal.Container>
-        </Modal.Scrim>
+
+                    <RadioButton.Item
+                        label={translate("ChangeTheme_dark")}
+                        value={"dark"}
+                        style={{ paddingHorizontal: 0 }}
+                    />
+                </RadioButton.Group>
+            </Dialog.Content>
+
+            <Dialog.Actions>
+                <Button
+                    children={translate("cancel")}
+                    onPress={navigation.goBack}
+                />
+
+                <Button
+                    children={translate("ok")}
+                    onPress={() => {
+                        switchTheme(selectedTheme)
+                        navigation.goBack()
+                    }}
+                />
+            </Dialog.Actions>
+        </Dialog>
     )
 }
