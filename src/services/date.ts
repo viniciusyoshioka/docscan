8ea9e7a7efa1a3ee5f25235ece9f52bdc831c5c8
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-03-12 13:43:06 -0300

    update: DateService now accepts number and Date as parameters

--- src/services/date.ts
+++ src/services/date.ts
@@ -1,10 +1,23 @@
 
 export class DateService {
 
+
+    private static normalizeDate(dateObject?: number | Date): Date {
+        if (dateObject === undefined) {
+            dateObject = new Date()
+        }
+        if (typeof dateObject === "number") {
+            dateObject = new Date(dateObject)
+        }
+        return dateObject
+    }
+
+
     /**
-     * Get the date in format YYYY-MM-DD
+     * Get the date in format `YYYY-MM-DD`
      */
-    static getDate(dateObject = new Date()): string {
+    static getDate(dateObject?: number | Date): string {
+        dateObject = this.normalizeDate(dateObject)
         const year = dateObject.getFullYear()
         const month = (dateObject.getMonth() + 1).toString().padStart(2, "0")
         const day = dateObject.getDate().toString().padStart(2, "0")
@@ -12,17 +25,19 @@ export class DateService {
     }
 
     /**
-     * Get the date in format DD/MM/YYYY according to the user's location
+     * Get the date in format `DD/MM/YYYY` according to the user's location
      */
-    static getLocaleDate(dateObject = new Date()): string {
+    static getLocaleDate(dateObject?: number | Date): string {
+        dateObject = this.normalizeDate(dateObject)
         return dateObject.toLocaleDateString()
     }
 
 
     /**
-     * Get the time in format HH-MM-SS
+     * Get the time in format `HH-MM-SS`
      */
-    static getTime(dateObject = new Date()): string {
+    static getTime(dateObject?: number | Date): string {
+        dateObject = this.normalizeDate(dateObject)
         const hour = dateObject.getHours().toString().padStart(2, "0")
         const minute = dateObject.getMinutes().toString().padStart(2, "0")
         const second = dateObject.getSeconds().toString().padStart(2, "0")
@@ -30,9 +45,10 @@ export class DateService {
     }
 
     /**
-     * Get the time in format HH:MM or HH:MM:SS according to the user's location
+     * Get the time in format `HH:MM` or `HH:MM:SS` according to the user's location
      */
-    static getLocaleTime(dateObject = new Date(), hasSeconds = true): string {
+    static getLocaleTime(dateObject?: number | Date, hasSeconds = true): string {
+        dateObject = this.normalizeDate(dateObject)
         return dateObject.toLocaleTimeString([], {
             hour: "2-digit",
             minute: "2-digit",
@@ -42,19 +58,19 @@ export class DateService {
 
 
     /**
-     * Get the date and time in format YYYY-MM-DD HH-MM-SS
+     * Get the date and time in format `YYYY-MM-DD HH-MM-SS`
      */
-    static getDateTime(dateObject = new Date()): string {
+    static getDateTime(dateObject?: number | Date): string {
         const date = this.getDate(dateObject)
         const time = this.getTime(dateObject)
         return `${date} ${time}`
     }
 
     /**
-     * Get the date and time in format DD/MM/YYYY HH:MM or DD/MM/YYYY HH:MM:SS
+     * Get the date and time in format `DD/MM/YYYY HH:MM` or `DD/MM/YYYY HH:MM:SS`
      * according to the user's location
      */
-    static getLocaleDateTime(dateObject = new Date(), hasSeconds = true): string {
+    static getLocaleDateTime(dateObject?: number | Date, hasSeconds = true): string {
         const date = this.getLocaleDate(dateObject)
         const time = this.getLocaleTime(dateObject, hasSeconds)
         return `${date} ${time}`
