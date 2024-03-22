9e1c8de45003353c9b0479357c8ff47d457dc871
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-03-22 19:40:49 -0300

    refactor: Improve readability of database schemas

--- src/database/schemas/ExportedDocument.ts
+++ src/database/schemas/ExportedDocument.ts
@@ -4,9 +4,19 @@ import { Realm } from "@realm/react"
 export const ExportedDocumentSchema: Realm.ObjectSchema = {
   name: "ExportedDocumentSchema",
   properties: {
-    id: { type: "objectId", indexed: true, default: () => new Realm.BSON.ObjectId() },
-    createdAt: { type: "int", default: Date.now },
-    modifiedAt: { type: "int", default: Date.now },
+    id: {
+      type: "objectId",
+      indexed: true,
+      default: () => new Realm.BSON.ObjectId(),
+    },
+    createdAt: {
+      type: "int",
+      default: Date.now,
+    },
+    modifiedAt: {
+      type: "int",
+      default: Date.now,
+    },
     name: "string",
   },
   primaryKey: "id",
--- src/database/schemas/ExportedDocumentPicture.ts
+++ src/database/schemas/ExportedDocumentPicture.ts
@@ -4,7 +4,11 @@ import { Realm } from "@realm/react"
 export const ExportedDocumentPictureSchema: Realm.ObjectSchema = {
   name: "ExportedDocumentPictureSchema",
   properties: {
-    id: { type: "objectId", indexed: true, default: () => new Realm.BSON.ObjectId() },
+    id: {
+      type: "objectId",
+      indexed: true,
+      default: () => new Realm.BSON.ObjectId(),
+    },
     fileName: "string",
     position: "int",
     belongsToDocument: "objectId",
