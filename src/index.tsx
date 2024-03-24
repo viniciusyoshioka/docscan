f95cef5292ead822ddc97b7e0cc74c2abfbaec3a
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-03-23 22:39:26 -0300

    refactor: Delete previous settings service

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import { KeyboardAvoidingView, KeyboardProvider } from "react-native-keyboard-co
 import { DocumentModelProvider, RealmProvider } from "@database"
 import { useKeepAwakeOnDev } from "@hooks"
 import { Router } from "@router"
-import { SettingsProvider } from "@services/settings"
 import { AppThemeProvider } from "@theme"
 
 
@@ -18,21 +17,19 @@ export function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <RealmProvider>
-        <SettingsProvider>
-          <DocumentModelProvider>
-            <AppThemeProvider>
-              <KeyboardProvider statusBarTranslucent={true}>
-                <KeyboardAvoidingView
-                  style={{ flex: 1 }}
-                  behavior={"padding"}
-                  keyboardVerticalOffset={-(StatusBar.currentHeight ?? 0)}
-                >
-                  <Router />
-                </KeyboardAvoidingView>
-              </KeyboardProvider>
-            </AppThemeProvider>
-          </DocumentModelProvider>
-        </SettingsProvider>
+        <DocumentModelProvider>
+          <AppThemeProvider>
+            <KeyboardProvider statusBarTranslucent={true}>
+              <KeyboardAvoidingView
+                style={{ flex: 1 }}
+                behavior={"padding"}
+                keyboardVerticalOffset={-(StatusBar.currentHeight ?? 0)}
+              >
+                <Router />
+              </KeyboardAvoidingView>
+            </KeyboardProvider>
+          </AppThemeProvider>
+        </DocumentModelProvider>
       </RealmProvider>
     </GestureHandlerRootView>
   )
--- src/services/settings/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./camera"
-export * from "./constants"
-export * from "./provider"
-export * from "./types"
