290fbd0b2631b3cf33238943d19c3f78f1c5461f
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-03-29 20:09:59 -0300

    chore: Add DatabaseProvider to App

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from "react-native"
 import { GestureHandlerRootView } from "react-native-gesture-handler"
 import { KeyboardAvoidingView, KeyboardProvider } from "react-native-keyboard-controller"
 
-import { DocumentModelProvider, RealmProvider } from "@database"
+import { DatabaseProvider, DocumentModelProvider, RealmProvider } from "@database"
 import { useKeepAwakeOnDev } from "@hooks"
 import { Router } from "@router"
 import { AppThemeProvider } from "@theme"
@@ -17,19 +17,21 @@ export function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <RealmProvider>
-        <DocumentModelProvider>
-          <AppThemeProvider>
-            <KeyboardProvider statusBarTranslucent={true}>
-              <KeyboardAvoidingView
-                style={{ flex: 1 }}
-                behavior={"padding"}
-                keyboardVerticalOffset={-(StatusBar.currentHeight ?? 0)}
-              >
-                <Router />
-              </KeyboardAvoidingView>
-            </KeyboardProvider>
-          </AppThemeProvider>
-        </DocumentModelProvider>
+        <DatabaseProvider>
+          <DocumentModelProvider>
+            <AppThemeProvider>
+              <KeyboardProvider statusBarTranslucent={true}>
+                <KeyboardAvoidingView
+                  style={{ flex: 1 }}
+                  behavior={"padding"}
+                  keyboardVerticalOffset={-(StatusBar.currentHeight ?? 0)}
+                >
+                  <Router />
+                </KeyboardAvoidingView>
+              </KeyboardProvider>
+            </AppThemeProvider>
+          </DocumentModelProvider>
+        </DatabaseProvider>
       </RealmProvider>
     </GestureHandlerRootView>
   )
