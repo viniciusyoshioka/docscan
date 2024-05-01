5521e6a5135ed2195c32cc07d43ec014f2232e1d
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-05-01 16:25:51 -0300

    test: Add tests for settings module

---
+++ src/lib/settings/__tests__/camera/utils.test.ts
@@ -0,0 +1,19 @@
+import { CameraRatio, getCameraRatioNumber } from "@lib/settings/camera"
+
+
+describe("Test camera utils from settings module", () => {
+  it("should return the correct ratio number for 4:3 ratio arg", () => {
+    const ratioNumber = getCameraRatioNumber("4:3")
+    expect(ratioNumber).toBeCloseTo(4 / 3)
+  })
+
+  it("should return the correct ratio number for 16:9 ratio arg", () => {
+    const ratioNumber = getCameraRatioNumber("16:9")
+    expect(ratioNumber).toBeCloseTo(16 / 9)
+  })
+
+  it("should throw when invalid ratio arg is passed", () => {
+    expect(() => getCameraRatioNumber("invalid" as CameraRatio))
+      .toThrow("Invalid ratio provided")
+  })
+})
