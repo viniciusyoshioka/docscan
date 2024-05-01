db8b1710a0189091466034e7e408f2d2ec5ac0fa
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-05-01 14:54:11 -0300

    refactor: Change settings store implementation to a builder function to allow dependency injection

--- src/lib/settings/store.ts
+++ src/lib/settings/store.ts
@@ -1,28 +1,32 @@
 import { merge } from "lodash"
 import { MMKV } from "react-native-mmkv"
 import { create } from "zustand"
-import { createJSONStorage, persist } from "zustand/middleware"
+import { StateStorage, createJSONStorage, persist } from "zustand/middleware"
 
 import { MMKVStorage } from "./MMKVStorage"
 import { defaultSettings } from "./default"
 import { SettingsStore } from "./types"
 
 
-export const useSettings = create<SettingsStore>()(persist(
-  set => ({
-    settings: defaultSettings,
-    setSettings: newSettings => {
-      set(state => ({
-        settings: merge(state.settings, newSettings),
-      }))
-    },
-  }),
-  {
-    name: "settings-storage",
-    version: 1,
-    storage: createJSONStorage(() => {
-      const settingsStorage = new MMKV({ id: "settings-storage" })
-      return new MMKVStorage(settingsStorage)
+export function createSettingsHook(stateStorage: StateStorage) {
+  return create<SettingsStore>()(persist(
+    set => ({
+      settings: defaultSettings,
+      setSettings: newSettings => {
+        set(state => ({
+          settings: merge(state.settings, newSettings),
+        }))
+      },
     }),
-  },
-))
+    {
+      name: "settings-storage",
+      version: 1,
+      storage: createJSONStorage(() => stateStorage),
+    },
+  ))
+}
+
+
+const mmkvSettingsStorage = new MMKV({ id: "settings-storage" })
+const mmkvStateStorage = new MMKVStorage(mmkvSettingsStorage)
+export const useSettings = createSettingsHook(mmkvStateStorage)
