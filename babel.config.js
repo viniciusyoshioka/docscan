bdb840e11144d5dca667b6b6886c17b97338edb8
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-05-02 14:45:23 -0300

    deps: Add missing @babel/preset-typescript configuration for jest

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  presets: ["module:@react-native/babel-preset"],
+  presets: [
+    "module:@react-native/babel-preset",
+    "@babel/preset-typescript",
+  ],
   plugins: [
     [
       "module-resolver",
