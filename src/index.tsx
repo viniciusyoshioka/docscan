bc2c401054bd18044dae05fdc1862d4ea9f907c1
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-05-04 17:26:46 -0300

    chore: Delete previous document model from database

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from "react-native"
 import { GestureHandlerRootView } from "react-native-gesture-handler"
 import { KeyboardAvoidingView, KeyboardProvider } from "react-native-keyboard-controller"
 
-import { DatabaseProvider, DocumentModelProvider } from "@database"
+import { DatabaseProvider } from "@database"
 import { useKeepAwakeOnDev } from "@hooks"
 import { LoggerProvider } from "@lib/log"
 import { Router } from "@router"
@@ -18,21 +18,19 @@ export function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <DatabaseProvider>
-        <DocumentModelProvider>
-          <LoggerProvider>
-            <AppThemeProvider>
-              <KeyboardProvider statusBarTranslucent={true}>
-                <KeyboardAvoidingView
-                  style={{ flex: 1 }}
-                  behavior={"padding"}
-                  keyboardVerticalOffset={-(StatusBar.currentHeight ?? 0)}
-                >
-                  <Router />
-                </KeyboardAvoidingView>
-              </KeyboardProvider>
-            </AppThemeProvider>
-          </LoggerProvider>
-        </DocumentModelProvider>
+        <LoggerProvider>
+          <AppThemeProvider>
+            <KeyboardProvider statusBarTranslucent={true}>
+              <KeyboardAvoidingView
+                style={{ flex: 1 }}
+                behavior={"padding"}
+                keyboardVerticalOffset={-(StatusBar.currentHeight ?? 0)}
+              >
+                <Router />
+              </KeyboardAvoidingView>
+            </KeyboardProvider>
+          </AppThemeProvider>
+        </LoggerProvider>
       </DatabaseProvider>
     </GestureHandlerRootView>
   )
