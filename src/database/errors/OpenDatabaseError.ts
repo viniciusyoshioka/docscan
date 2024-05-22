4d44f1fec88873b30bb8be27ae2b8cda5d0d43a9
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-05-22 14:15:34 -0300

    refactor: Add custom error message for OpenDatabaseError

--- src/database/errors/OpenDatabaseError.ts
+++ src/database/errors/OpenDatabaseError.ts
@@ -2,8 +2,20 @@ import { BaseDatabaseError } from "./BaseDatabaseError"
 
 
 export class OpenDatabaseError extends BaseDatabaseError {
-  constructor(message: string, options?: ErrorOptions) {
-    super(message, options)
+
+
+  protected databaseName: string
+
+
+  constructor(databaseName: string, message: string, options?: ErrorOptions) {
+    const errorMessage = `Failed to open ${databaseName} database: ${message}`
+    super(errorMessage, options)
     this.name = "OpenDatabaseError"
+    this.databaseName = databaseName
+  }
+
+
+  getDatabaseName(): string {
+    return this.databaseName
   }
 }
