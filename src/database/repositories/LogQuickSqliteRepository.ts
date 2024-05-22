75521caa3ed06acf5c34e2bda54be3e82b345c7b
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-05-22 13:38:51 -0300

    feat: Add LogQuickSqliteRepository

---
+++ src/database/repositories/LogQuickSqliteRepository.ts
@@ -0,0 +1,37 @@
+import { QuickSQLiteConnection } from "react-native-quick-sqlite"
+
+import { stringifyError } from "@utils"
+import { Log } from "../entities"
+import { UnknownDatabaseError } from "../errors"
+import { QuickSqliteProvider } from "../providers"
+import { WithId } from "../types"
+import { LogRepository } from "./interfaces"
+
+
+export class LogQuickSqliteRepository implements LogRepository {
+
+
+  private database: QuickSQLiteConnection
+
+
+  constructor(quickSqliteProvider: QuickSqliteProvider) {
+    this.database = quickSqliteProvider.getDatabase()
+  }
+
+
+  async insert(log: Log): Promise<WithId<Log>> {
+    try {
+      const { insertId } = this.database.execute(`
+        INSERT INTO logs (code, message, timestamp) VALUES (?, ?, ?);
+      `, [log.code, log.message, log.timestamp])
+
+      return {
+        ...log,
+        id: String(insertId),
+      }
+    } catch (error) {
+      const stringifiedError = stringifyError(error)
+      throw new UnknownDatabaseError(stringifiedError)
+    }
+  }
+}
--- src/database/repositories/index.ts
+++ src/database/repositories/index.ts
@@ -1,1 +1,2 @@
+export * from "./LogQuickSqliteRepository"
 export * from "./interfaces"
