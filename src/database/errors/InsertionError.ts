7ef7ae67078a9de88c75886878dbaf59aa725343
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-05-22 17:31:57 -0300

    refactor: Remove message param from InsertionError constructor

--- src/database/errors/InsertionError.ts
+++ src/database/errors/InsertionError.ts
@@ -7,8 +7,8 @@ export class InsertionError extends BaseDatabaseError {
   protected entity: string
 
 
-  constructor(entity: string, message: string, options?: ErrorOptions) {
-    const errorMessage = `Failed to insert data for ${entity}: ${message}`
+  constructor(entity: string, options?: ErrorOptions) {
+    const errorMessage = `Failed to insert data for ${entity}`
     super(errorMessage, options)
     this.name = "InsertionError"
     this.entity = entity
