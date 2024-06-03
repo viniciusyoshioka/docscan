50dc53260fd61c06be1a9dc4bc18318701e36f18
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-06-02 21:50:47 -0300

    refactor: Using get keyword for InvalidLogCodeError getter

--- src/lib/log/errors/InvalidLogCodeError.ts
+++ src/lib/log/errors/InvalidLogCodeError.ts
@@ -4,18 +4,18 @@ import { LogCode } from "@database"
 export class InvalidLogCodeError extends Error {
 
 
-  private givenCode: LogCode
+  private _givenCode: LogCode
 
 
   constructor(givenCode: LogCode, message?: string, options?: ErrorOptions) {
     const errorMessage = message ?? `Invalid log code provided: ${givenCode}`
     super(errorMessage, options)
     this.name = "InvalidLogCodeError"
-    this.givenCode = givenCode
+    this._givenCode = givenCode
   }
 
 
-  public getGivenCode(): LogCode {
-    return this.givenCode
+  get givenCode(): LogCode {
+    return this._givenCode
   }
 }
