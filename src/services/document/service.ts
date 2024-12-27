5044a656385907ec8f941417b68bd89d5464b130
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-12-26 23:37:36 -0300

    refactor: Format and replace date formatter in DocumentService

--- src/services/document/service.ts
+++ src/services/document/service.ts
@@ -2,17 +2,30 @@ import { NativeModules } from "react-native"
 import RNFS from "react-native-fs"
 import { v4 as uuid4 } from "uuid"
 
+import { StandardDateFormatter } from "@libs/date-formatter"
 import { translate } from "@locales"
-import { Constants } from "../constant"
-import { DateService } from "../date"
+import { Constants } from "@services/constant"
 import { DocumentServiceType } from "./native-types"
-import { DocumentDataToExport, PictureDataToCopy, PictureDataToDelete, PictureDataToMove } from "./types"
+import {
+  DocumentDataToExport,
+  PictureDataToCopy,
+  PictureDataToDelete,
+  PictureDataToMove,
+} from "./types"
 
 
 const NativeDocumentService = NativeModules.DocumentService as DocumentServiceType
 
 
 export class DocumentService {
+
+
+  private static dateFormatter = new StandardDateFormatter({
+    separators: {
+      date: "",
+      time: "",
+    },
+  })
 
 
   static getNewName(): string {
@@ -40,9 +53,8 @@ export class DocumentService {
 
   static getExportedDocumentPath(): string {
     const name = translate("document_exportedDocumentName")
-    const date = DateService.getDate().replaceAll("-", "")
-    const time = DateService.getTime().replaceAll("-", "")
-    return `${Constants.fullPathExported}/${name} ${date}_${time}.zip`
+    const dateTime = this.dateFormatter.formatDateTime().replaceAll(" ", "_")
+    return `${Constants.fullPathExported}/${name} ${dateTime}.zip`
   }
 
   static getTemporaryImportedPicturePath(fileName: string): string {
@@ -72,21 +84,30 @@ export class DocumentService {
   static deletePicturesService(data: PictureDataToDelete) {
     NativeDocumentService.deletePictures({
       pictures: data.pictures,
-      notificationTitle: data.notificationTitle ?? translate("document_notification_deletingImages_title"),
+      notificationTitle: (
+        data.notificationTitle
+        ?? translate("document_notification_deletingImages_title")
+      ),
     })
   }
 
   static copyPicturesService(data: PictureDataToCopy) {
     NativeDocumentService.copyPictures({
       pictures: data.pictures,
-      notificationTitle: data.notificationTitle ?? translate("document_notification_copyingImages_title"),
+      notificationTitle: (
+        data.notificationTitle
+        ?? translate("document_notification_copyingImages_title")
+      ),
     })
   }
 
   static movePicturesService(data: PictureDataToMove) {
     NativeDocumentService.movePictures({
       pictures: data.pictures,
-      notificationTitle: data.notificationTitle ?? translate("document_notification_movingImages_title"),
+      notificationTitle: (
+        data.notificationTitle
+        ?? translate("document_notification_movingImages_title")
+      ),
     })
   }
 
@@ -96,7 +117,10 @@ export class DocumentService {
       databasePath: data.databasePath,
       pathZipTo: data.pathZipTo,
       pathExportedDocument: data.pathExportedDocument,
-      notificationTitle: data.notificationTitle ?? translate("document_notification_exportingDocuments_title"),
+      notificationTitle: (
+        data.notificationTitle
+        ?? translate("document_notification_exportingDocuments_title")
+      ),
     })
   }
 }
