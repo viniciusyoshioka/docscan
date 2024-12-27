3bbfe38ea4156b1ccfe7791cd714d6a497a7cab0
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-12-26 23:09:08 -0300

    chore: Add tsconfig.eslint.json config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
 /** @type {import('eslint').ESLint.ConfigData} */
 module.exports = {
-    extends: "@vinicius1313/eslint-config",
-    root: true,
-    parserOptions: {
-        tsconfigRootDir: __dirname,
-    },
+  extends: "@vinicius1313/eslint-config",
+  root: true,
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: "tsconfig.eslint.json",
+  },
 }
