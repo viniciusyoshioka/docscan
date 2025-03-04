c015ed690c04e36be5a3038eea8a8dd949baa137
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2025-03-04 11:47:04 -0300

    refactor: Remove error handling from `useRequestNotificationPermission` as it should not throw

--- src/screen/Home/hooks/useRequestNotificationPermission.ts
+++ src/screen/Home/hooks/useRequestNotificationPermission.ts
@@ -1,32 +1,22 @@
 import { useCallback, useEffect } from "react"
 import { Alert } from "react-native"
 
-import { useLogger } from "@libs/logger"
 import { translate } from "@locales"
 import { getNotificationPermission } from "@services/permission"
-import { stringifyError } from "@utils"
 
 
 export function useRequestNotificationPermission() {
 
 
-  const logger = useLogger()
-
-
   const requestPermissions = useCallback(async () => {
-    try {
-      const hasPermission = await getNotificationPermission()
-      if (!hasPermission) {
-        Alert.alert(
-          translate("Home_alert_notificationPermissionDenied_title"),
-          translate("Home_alert_notificationPermissionDenied_text"),
-        )
-      }
-    } catch (error) {
-      const errorMessage = stringifyError(error)
-      logger.error(`Error requesting notification permission: ${errorMessage}`)
+    const hasPermission = await getNotificationPermission()
+    if (!hasPermission) {
+      Alert.alert(
+        translate("Home_alert_notificationPermissionDenied_title"),
+        translate("Home_alert_notificationPermissionDenied_text"),
+      )
     }
-  }, [logger])
+  }, [])
 
 
   useEffect(() => {
