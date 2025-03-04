a86b055e26d4b9e18d5645bb7a11b546b4b0787e
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2025-03-04 16:40:01 -0300

    feat: Implement `useEditDocument`

--- src/screen/Camera/hooks/useEditDocument.ts
+++ src/screen/Camera/hooks/useEditDocument.ts
@@ -1,12 +1,23 @@
+import { useNavigation, useRoute } from "@react-navigation/native"
 import { useCallback } from "react"
+
+import { NavigationProps, RouteProps } from "@router"
 
 
 export function useEditDocument() {
 
 
-  const editDocument = useCallback(() => {
+  const navigation = useNavigation<NavigationProps<"Camera">>()
+  const { params } = useRoute<RouteProps<"Camera">>()
 
-  }, [])
+
+  const editDocument = useCallback(() => {
+    if (params?.action === "add-picture") {
+      navigation.goBack()
+    } else {
+      navigation.replace("EditDocument")
+    }
+  }, [params, navigation])
 
 
   return editDocument
