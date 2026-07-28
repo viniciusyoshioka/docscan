0bc933d496598a5316d759dae0e6740a5ae04b70
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2026-07-28 09:35:14 -0300

    feat: Use `Info` module for database names

--- src/database/typeorm/datasources/app.datasource.ts
+++ src/database/typeorm/datasources/app.datasource.ts
@@ -1,12 +1,14 @@
 import { typeORMDriver } from 'react-native-nitro-sqlite'
 import { DataSource } from 'typeorm'
 
+import { Info } from '@modules/info'
+import { DatabaseName } from '../../database'
 import { APP_MIGRATIONS } from '../migrations'
 
 
 export const AppDataSource = new DataSource({
   type: 'react-native',
-  database: 'app-database.sqlite',
+  database: Info.database[DatabaseName.APP].fileName,
   location: 'default',
   driver: typeORMDriver,
   entities: [],
--- src/database/typeorm/datasources/log.datasource.ts
+++ src/database/typeorm/datasources/log.datasource.ts
@@ -1,13 +1,15 @@
 import { typeORMDriver } from 'react-native-nitro-sqlite'
 import { DataSource } from 'typeorm'
 
+import { Info } from '@modules/info'
+import { DatabaseName } from '../../database'
 import { TypeOrmLogEntity } from '../entities'
 import { LOG_MIGRATIONS } from '../migrations'
 
 
 export const LogDataSource = new DataSource({
   type: 'react-native',
-  database: 'log-database.sqlite',
+  database: Info.database[DatabaseName.LOG].fileName,
   location: 'default',
   driver: typeORMDriver,
   entities: [
