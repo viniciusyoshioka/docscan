bf3eab102ef859cc2cf278313fcc30bcef245e67
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2026-07-28 16:21:20 -0300

    feat: Add camera settings to settings module

--- src/modules/settings/settings.constants.ts
+++ src/modules/settings/settings.constants.ts
@@ -1,7 +1,17 @@
 import type { Settings } from './settings.types.ts'
-import { Theme } from './settings.types.ts'
+import {
+  CameraFlash,
+  CameraPosition,
+  CameraRatio,
+  Theme,
+} from './settings.types.ts'
 
 
 export const DEFAULT_SETTINGS: Settings = {
+  camera: {
+    flash: CameraFlash.OFF,
+    position: CameraPosition.BACK,
+    ratio: CameraRatio.RATIO_4_3,
+  },
   theme: Theme.AUTO,
 }
--- src/modules/settings/settings.types.ts
+++ src/modules/settings/settings.types.ts
@@ -1,6 +1,23 @@
 import type { PartialDeep } from 'type-fest'
 
 
+export enum CameraFlash {
+  AUTO = 'auto',
+  ON = 'on',
+  OFF = 'off',
+}
+
+export enum CameraPosition {
+  FRONT = 'front',
+  BACK = 'back',
+}
+
+export enum CameraRatio {
+  RATIO_4_3 = '4:3',
+  RATIO_16_9 = '16:9',
+}
+
+
 export enum Theme {
   AUTO = 'AUTO',
   LIGHT = 'LIGHT',
@@ -9,6 +26,11 @@ export enum Theme {
 
 
 export interface Settings {
+  camera: {
+    flash: CameraFlash
+    position: CameraPosition
+    ratio: CameraRatio
+  }
   theme: Theme
 }
 
