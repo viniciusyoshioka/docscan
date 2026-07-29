e0c944fedafbef8985ebd47a908e50d660bc917d
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2026-07-29 17:13:01 -0300

    fix: Add file extension in import path

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import 'react-native-get-random-values'
 import 'reflect-metadata'
 
 import { name as appName } from './app.json'
-import { App } from './src/app'
+import { App } from './src/app.tsx'
 
 
 AppRegistry.registerComponent(appName, () => App)
