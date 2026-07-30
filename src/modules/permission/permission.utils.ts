ca62f26cd92dda94cfc66f9cdf1c8cfbf20cd246
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2026-07-30 00:08:41 -0300

    feat: Add `PermissionUtils`

---
+++ src/modules/permission/permission.utils.ts
@@ -0,0 +1,36 @@
+import { UsePermissionStatus } from './hooks'
+
+
+export class PermissionUtils {
+
+
+  private static readonly LOADING_STATUS = new Set([
+    UsePermissionStatus.IS_CHECKING,
+    UsePermissionStatus.IS_REQUESTING,
+  ])
+  private static readonly ERROR_STATUS = new Set([
+    UsePermissionStatus.ERROR_CHECKING,
+    UsePermissionStatus.ERROR_REQUESTING,
+  ])
+  private static readonly DENIED_STATUS = new Set([
+    UsePermissionStatus.DENIED_CANNOT_REQUEST,
+    UsePermissionStatus.DENIED_CAN_REQUEST,
+  ])
+
+
+  static isLoading(status: UsePermissionStatus): boolean {
+    return this.LOADING_STATUS.has(status)
+  }
+
+  static hasError(status: UsePermissionStatus): boolean {
+    return this.ERROR_STATUS.has(status)
+  }
+
+  static isDenied(status: UsePermissionStatus): boolean {
+    return this.DENIED_STATUS.has(status)
+  }
+
+  static isGranted(status: UsePermissionStatus): boolean {
+    return status === UsePermissionStatus.GRANTED
+  }
+}
