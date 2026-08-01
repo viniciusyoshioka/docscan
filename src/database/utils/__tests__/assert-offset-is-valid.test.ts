1ec09c3ebd3aa8fa92bef6548ddce9255c8aaf4e
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2026-08-01 16:05:50 -0300

    test: Add unit test for `assertOffsetIsValid`

---
+++ src/database/utils/__tests__/assert-offset-is-valid.test.ts
@@ -0,0 +1,57 @@
+import { assertOffsetIsValid } from '../assert-offset-is-valid.ts'
+
+
+function buildOffsetErrorMessage(offset: number): string {
+  return `Invalid value for offset: "${offset}"`
+}
+
+
+describe('assertOffsetIsValid', () => {
+  it('should not throw when offset is positive', () => {
+    const offset = 1
+
+    expect(
+      () => assertOffsetIsValid(offset),
+    ).not.toThrow()
+  })
+
+  it('should throw when offset is negative', () => {
+    const offset = -1
+
+    expect(
+      () => assertOffsetIsValid(offset),
+    ).toThrow(buildOffsetErrorMessage(offset))
+  })
+
+  it('should throw when offset is zero', () => {
+    const offset = 0
+
+    expect(
+      () => assertOffsetIsValid(offset),
+    ).toThrow(buildOffsetErrorMessage(offset))
+  })
+
+  it('should throw when offset is decimal number', () => {
+    const offset = 3.14
+
+    expect(
+      () => assertOffsetIsValid(offset),
+    ).toThrow(buildOffsetErrorMessage(offset))
+  })
+
+  it('should throw when offset is NaN', () => {
+    const offset = NaN
+
+    expect(
+      () => assertOffsetIsValid(offset),
+    ).toThrow(buildOffsetErrorMessage(offset))
+  })
+
+  it('should throw when offset is Infinity', () => {
+    const offset = Infinity
+
+    expect(
+      () => assertOffsetIsValid(offset),
+    ).toThrow(buildOffsetErrorMessage(offset))
+  })
+})
